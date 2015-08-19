c96716d206cf53d1ff25a4749b16c5fd30aa043e
Dmitriy Shekhovtsov <valorkin@gmail.com>
2015-08-19 15:48:41 +0300

    Update: fix for demo page

--- src/public/index.ts
+++ src/public/index.ts
@@ -18,15 +18,15 @@ import {TooltipDemo} from 'src/components/demo/tooltip-demo';
 @View({
   template: `
     <div></div>
-    <accordion-demo></accordion-demo>
-    <alert-demo></alert-demo>
-    <buttons-demo></buttons-demo>
-    <dropdown-demo></dropdown-demo>
-    <collapse-demo></collapse-demo>
-    <pagination-demo></pagination-demo>
-    <progressbar-demo></progressbar-demo>
-    <rating-demo></rating-demo>
-    <tooltip-demo></tooltip-demo>
+    <accordion-demo class="col-md-12"></accordion-demo>
+    <alert-demo class="col-md-12"></alert-demo>
+    <buttons-demo class="col-md-12"></buttons-demo>
+    <dropdown-demo class="col-md-12"></dropdown-demo>
+    <collapse-demo class="col-md-12"></collapse-demo>
+    <pagination-demo class="col-md-12"></pagination-demo>
+    <progressbar-demo class="col-md-12"></progressbar-demo>
+    <rating-demo class="col-md-12"></rating-demo>
+    <tooltip-demo class="col-md-12"></tooltip-demo>
   `,
   directives: [
     AlertDemo, AccordionDemo,
