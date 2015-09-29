a5728d293cb12e583a39d1dafd2a8875f673f81d
luis <luisbigvargas@gmail.com>
2015-09-29 00:42:17 -0400

    change getTime to millisecondsSinceEpoch

--- lib/datepicker/date-formatter.dart
+++ lib/datepicker/date-formatter.dart
@@ -2,6 +2,6 @@ import "package:moment.dart" as moment;
 
 class DateFormatter {
   String format(DateTime date, String format) {
-    return moment(date.getTime()).format(format);
+    return moment(date.millisecondsSinceEpoch).format(format);
   }
 }
