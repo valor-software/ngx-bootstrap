f21b95cd74b4f16c0e0434569e1b4d850e2bc5bd
jkuri <jkuri88@gmail.com>
2016-02-04 13:14:01 +0100

    chore(): Make it compatible with angular-cli

--- ng2-bootstrap.ts
+++ ng2-bootstrap.ts
@@ -1,3 +1,18 @@
+import {Accordion, AccordionPanel} from './components/accordion';
+import {Alert} from './components/alert';
+import {ButtonCheckbox, ButtonRadio} from './components/buttons';
+import {Carousel, Slide} from './components/carousel';
+import {Collapse} from './components/collapse';
+import {DatePicker} from './components/datepicker';
+import {Dropdown} from './components/dropdown';
+import {Pagination, Pager} from './components/pagination';
+import {Progressbar, Progress} from './components/progressbar';
+import {Rating} from './components/rating';
+import {Tabset, Tab, TabHeading} from './components/tabs';
+import {Timepicker} from './components/timepicker';
+import {Tooltip} from './components/tooltip';
+import {Typeahead} from './components/typeahead';
+
 export * from  './components/accordion';
 export * from  './components/alert';
 export * from  './components/buttons';
@@ -16,3 +31,24 @@ export * from  './components/typeahead';
 export * from  './components/position'
 export * from  './components/common'
 export * from  './components/ng2-bootstrap-config';
+
+export default {
+  directives: [Dropdown, Progress, Tab, TabHeading],
+  components: [
+    Accordion,
+    AccordionPanel,
+    ButtonCheckbox,
+    ButtonRadio,
+    Carousel,
+    Slide,
+    Collapse,
+    DatePicker,
+    Pagination,
+    Pager,
+    Rating,
+    Tabset,
+    Timepicker,
+    Tooltip,
+    Typeahead
+  ]
+}
