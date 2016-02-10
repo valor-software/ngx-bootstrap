c465610b0c9fa6389968df93d7793277bf491e0e
Panchenko Vyacheslav <vyacheslav.panchenko@valor-software.com>
2016-02-10 12:28:58 +0200

    feat(tabs): removable tabs

--- components/tabs/tabset.component.ts
+++ components/tabs/tabset.component.ts
@@ -15,6 +15,9 @@ import {Tab} from './tab.directive';
             [class.active]="tabz.active" [class.disabled]="tabz.disabled"
             (click)="tabz.active = true">
             <span [ngTransclude]="tabz.headingRef">{{tabz.heading}}</span>
+            <span [hidden]="!tabz.removable">
+              <span (click)="$event.preventDefault(); removeTab(tabz);" class="glyphicon glyphicon-remove-circle"></span>
+            </span>
           </a>
         </li>
     </ul>
@@ -84,12 +87,45 @@ export class Tabset implements OnInit {
       return;
     }
     // Select a new tab if the tab to be removed is selected and not destroyed
-    if (tab.active && this.tabs.length > 1) {
-      // If this is the last tab, select the previous tab. else, the next tab.
-      let newActiveIndex = index === this.tabs.length - 1 ? index - 1 : index + 1;
+    if (tab.active && this.hasAvailableTabs(index)) {
+      let newActiveIndex = this.getClosestTabIndex(index);
       this.tabs[newActiveIndex].active = true;
     }
 
-    this.tabs.slice(index, 1);
+    tab.removed.emit(tab);
+    this.tabs.splice(index, 1);
+  }
+
+  private getClosestTabIndex (index:number):number {
+    let tabsLength = this.tabs.length;
+    if (!tabsLength) {
+      return -1;
+    }
+
+    for (let step = 1; step <= tabsLength; step += 1) {
+      let prevIndex = index - step;
+      let nextIndex = index + step;
+      if (this.tabs[prevIndex] && !this.tabs[prevIndex].disabled) {
+        return prevIndex;
+      }
+      if (this.tabs[nextIndex] && !this.tabs[nextIndex].disabled) {
+        return nextIndex;
+      }
+    }
+    return -1;
+  }
+
+  private hasAvailableTabs (index:number) {
+    let tabsLength = this.tabs.length;
+    if (!tabsLength) {
+      return false;
+    }
+
+    for (let i = 0; i < tabsLength; i += 1) {
+      if (!this.tabs[i].disabled && i !== index) {
+        return true;
+      }
+    }
+    return false;
   }
 }
