d02b8f99aad343be8bb098b3dc5268fa81f6572a
Maxim Ugnichenko <ugmaxie@gmail.com>
2016-04-21 19:03:17 +0300

    test(alert): test alert component
    Alert component is tested for default class and dismissble option.

--- components/alert/alert.component.spec.ts
+++ components/alert/alert.component.spec.ts
@@ -1,13 +1,36 @@
-import {it, inject, beforeEachProviders} from 'angular2/testing';
+import {it, beforeEach, injectAsync, TestComponentBuilder, ComponentFixture} from 'angular2/testing';
 import {Alert} from './alert.component';
 
-describe('Alert', () => {
-  beforeEachProviders(() => [
-    Alert
-  ]);
+describe('Component: Alert', () => {
+    let fixture:ComponentFixture, context:any;
+    const overTemplate = `
+                  <div class="alert" role="alert" [ngClass]="classes" *ngIf="!closed">
+                    <button *ngIf="dismissible" type="button" class="close" (click)="onClose()" (touch)="onClose()">
+                        <span aria-hidden="true">&times;</span>
+                        <span class="sr-only">Close</span>
+                    </button>
+                  </div>
+            `;
 
-  it('should have default type', inject([Alert], (alert:Alert) => {
-    expect(alert.type)
-      .toEqual('warning');
-  }));
+    beforeEach(injectAsync([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+        return tcb
+            .overrideTemplate(Alert, overTemplate)
+            .createAsync(Alert)
+            .then((f:ComponentFixture) => {
+                fixture = f;
+                context = fixture.debugElement.componentInstance;
+            });
+    }));
+
+    it('should have a default type alert-warning', () => {
+        context.ngOnInit();
+        expect(context.classes[0]).toEqual(`alert-warning`);
+    });
+
+    it('should have class dismissible if dismissible=true', () => {
+        context.dismissible = true;
+        context.ngOnInit();
+        expect(context.classes.length).toEqual(2);
+        expect(context.classes[1]).toEqual(`alert-dismissible`);
+    });
 });
