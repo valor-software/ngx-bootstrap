ebaf74e61d62bf248925662c629714db83f0efa7
Maxim Ugnichenko <ugmaxie@gmail.com>
2016-04-22 18:54:00 +0300

    test(alert): test alert dismissible, dismissOnTimeout, onClose
    Tested alert dismissible, dismissOnTimeout and onClose options.

--- components/alert/alert.component.spec.ts
+++ components/alert/alert.component.spec.ts
@@ -24,6 +24,7 @@ describe('Component: Alert', () => {
 
     it('should have a default type alert-warning', () => {
         context.ngOnInit();
+        expect(context.type).toEqual(`warning`);
         expect(context.classes[0]).toEqual(`alert-warning`);
     });
 
@@ -32,5 +33,23 @@ describe('Component: Alert', () => {
         context.ngOnInit();
         expect(context.classes.length).toEqual(2);
         expect(context.classes[1]).toEqual(`alert-dismissible`);
+    });
+
+    it('should be dismissed by timeout', (done:() => void) => {
+        context.dismissOnTimeout = 1000;
+        context
+            .close
+            .subscribe(() => {
+                expect(context.closed).toBeTruthy();
+                done();
+            });
+        context.ngOnInit();
+    });
+
+    it('should be closed by public method onClose', () => {
+        context.ngOnInit();
+        expect(context.closed).toBeFalsy();
+        context.onClose();
+        expect(context.closed).toBeTruthy();
     });
 });
