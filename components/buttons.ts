67ee5b5ef337b91b7a33b8cb0ff854e2b693b099
Dmitriy Shekhovtsov <valorkin@gmail.com>
2016-04-28 14:24:41 +0300

    fix(buttons): had incorrect import statement which breaks .d.ts and import

--- components/buttons.ts
+++ components/buttons.ts
@@ -1,6 +1,6 @@
-import {ButtonCheckboxDirective} from './buttons/button-checkbox.directive.ts';
-import {ButtonRadioDirective} from './buttons/button-radio.directive.ts';
+import {ButtonCheckboxDirective} from './buttons/button-checkbox.directive';
+import {ButtonRadioDirective} from './buttons/button-radio.directive';
 
-export {ButtonCheckboxDirective} from './buttons/button-checkbox.directive.ts';
-export {ButtonRadioDirective} from './buttons/button-radio.directive.ts';
+export {ButtonCheckboxDirective} from './buttons/button-checkbox.directive';
+export {ButtonRadioDirective} from './buttons/button-radio.directive';
 export const BUTTON_DIRECTIVES:Array<any> = [ButtonCheckboxDirective, ButtonRadioDirective];
