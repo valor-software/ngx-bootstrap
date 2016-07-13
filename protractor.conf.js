eacd64add54c2978e223f556b5da65df62528463
SergeyKuryatnick <Sergey.kuryatnick@valor-software.com>
2016-07-13 15:58:12 +0300

    Simple e2e test. Test on Carousel page is failed(TimedOut)

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,7 +1,27 @@
-/**
- * @author: @AngularClass
- */
 'use strict';
 
-// look in ./config for protractor.conf.js
-module.exports.config = require('./config/protractor.conf.js').config;
+const env = require('./environment.js');
+
+exports.config = {
+  seleniumAddress: env.seleniumAddress,
+
+  useAllAngular2AppRoots: true,
+
+  baseUrl: 'http://localhost:3000/',
+
+  capabilities: env.capabilities,
+
+  specs: [
+    './e2e/test_e2e.ts'
+  ],
+
+  framework: 'jasmine',
+
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 80000
+  }
+};
+
+// // look in ./config for protractor.conf.js
+// module.exports.config = require('./config/protractor.conf.js').config;
