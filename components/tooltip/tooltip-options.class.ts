9070125f7d6635a76fed569b21bcd2b14d90f7bd
Adrian Faciu <adrian.faciu@gmail.com>
2016-07-15 01:39:19 +0300

    feat(tooltip): tooltip html content (#724)
    * Implemented html content for tooltip

--- components/tooltip/tooltip-options.class.ts
+++ components/tooltip/tooltip-options.class.ts
@@ -6,6 +6,8 @@ export class TooltipOptions {
   public popupClass:string;
   public animation:boolean;
   public isOpen:boolean;
+  public content:string;
+  public htmlContent:string;
 
   public constructor(options:Object) {
     Object.assign(this, options);
