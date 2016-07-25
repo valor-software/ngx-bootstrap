527273b4a8ebcb941b8027fb13085e35d1e938ba
Oleksandr Telnov <oleksandr.telnov@valor-software.com>
2016-07-25 13:06:46 +0300

    feat(collpase): add collapsed and expanded events to the collapse directive #576

--- demo/components/collapse/collapse-demo.ts
+++ demo/components/collapse/collapse-demo.ts
@@ -11,4 +11,12 @@ let template = require('./collapse-demo.html');
 })
 export class CollapseDemoComponent {
   public isCollapsed:boolean = false;
+
+  public collapsed(event:any):void {
+    console.log(event);
+  }
+
+  public expanded(event:any):void {
+    console.log(event);
+  }
 }
