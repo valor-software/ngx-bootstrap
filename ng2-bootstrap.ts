12bd1e8711849607102b7ca9584ac25dbf3bb215
Marçal Juan Llaó <marcalj@gmail.com>
2016-08-12 11:48:04 +0200

    Create a global module to load all components at once

--- ng2-bootstrap.ts
+++ ng2-bootstrap.ts
@@ -36,8 +36,27 @@ export * from './components/position';
 export * from './components/common';
 export * from './components/ng2-bootstrap-config';
 
+export {AccordionModule} from './components/accordion/accordion.module';
+export {AlertModule} from './components/alert/alert.module';
+export {ButtonsModule} from './components/buttons/buttons.module';
+export {CarouselModule} from './components/carousel/carousel.module';
+export {CollapseModule} from './components/collapse/collapse.module';
+export {DatepickerModule} from './components/datepicker/datepicker.module';
+export {DropdownModule} from './components/dropdown/dropdown.module';
+export {ModalModule} from './components/modal/modal.module';
+export {PaginationModule} from './components/pagination/pagination.module';
+export {ProgressbarModule} from './components/progressbar/progressbar.module';
+export {RatingModule} from './components/rating/rating.module';
+export {TabsModule} from './components/tabs/tabs.module';
+export {TimepickerModule} from './components/timepicker/timepicker.module';
+export {TooltipModule} from './components/tooltip/tooltip.module';
+export {TypeaheadModule} from './components/typeahead/typeahead.module';
+export {ComponentsHelper} from './components/utils/components-helper.service';
+
+/** @deprecated */
 export const BS_VIEW_PROVIDERS: any[] = [{provide: ComponentsHelper, useClass:ComponentsHelper}];
 
+/** @deprecated */
 export default {
   directives: [
     AlertComponent,
@@ -60,3 +79,5 @@ export default {
     ComponentsHelper
   ]
 };
+
+export {Ng2BootstrapModule} from './components/all';
