a79342a2a43467106beae4a1f808e3bb8e58e85a
Oleksandr Telnov <oleksandr.telnov@valor-software.com>
2016-08-12 15:08:55 +0300

    rename global module file from all to index

--- components/index.ts
+++ components/index.ts
@@ -1,26 +1,30 @@
 import { NgModule } from '@angular/core';
 
-import { AccordionModule } from './accordion';
-import { AlertModule } from './alert';
-import { ButtonsModule } from './buttons';
-import { CarouselModule } from './carousel';
-import { CollapseModule } from './collapse';
-import { DatepickerModule } from './datepicker';
-import { DropdownModule } from './dropdown';
-import { ModalModule } from './modal';
-import { PaginationModule } from './pagination';
-import { ProgressbarModule } from './progressbar';
-import { RatingModule } from './rating';
-import { TabsModule } from './tabs';
-import { TimepickerModule } from './timepicker';
-import { TooltipModule } from './tooltip';
-import { TypeaheadModule } from './typeahead';
+import { AccordionModule } from './accordion/accordion.module';
+import { AlertModule } from './alert/alert.module';
+import { ButtonsModule } from './buttons/buttons.module';
+import { CarouselModule } from './carousel/carousel.module';
+import { CollapseModule } from './collapse/collapse.module';
+import { DatepickerModule } from './datepicker/datepicker.module';
+import { DropdownModule } from './dropdown/dropdown.module';
+import { ModalModule } from './modal/modal.module';
+import { PaginationModule } from './pagination/pagination.module';
+import { ProgressbarModule } from './progressbar/progressbar.module';
+import { RatingModule } from './rating/rating.module';
+import { TabsModule } from './tabs/tabs.module';
+import { TimepickerModule } from './timepicker/timepicker.module';
+import { TooltipModule } from './tooltip/tooltip.module';
+import { TypeaheadModule } from './typeahead/typeahead.module';
+import { ComponentsHelper } from './utils/components-helper.service';
 
 @NgModule({
   exports: [
     AccordionModule, AlertModule, ButtonsModule, CarouselModule, CollapseModule, DatepickerModule, DropdownModule,
-    ModalModule, PaginationModule, ProgressbarModule, RatingModule, TabsModule, TimepickerModule,
-    TooltipModule, TypeaheadModule
+    ModalModule, PaginationModule, ProgressbarModule, RatingModule, TabsModule, TimepickerModule, TooltipModule,
+    TypeaheadModule
+  ],
+  providers: [
+    {provide: ComponentsHelper, useClass: ComponentsHelper}
   ]
 })
 export class Ng2BootstrapModule {
--- ng2-bootstrap.ts
+++ ng2-bootstrap.ts
@@ -1,20 +1,20 @@
-import {ACCORDION_DIRECTIVES} from './components/accordion';
-import {AlertComponent} from './components/alert';
-import {BUTTON_DIRECTIVES} from './components/buttons';
-import {CAROUSEL_DIRECTIVES} from './components/carousel';
-import {CollapseDirective} from './components/collapse';
-import {DATEPICKER_DIRECTIVES} from './components/datepicker';
-import {DROPDOWN_DIRECTIVES} from './components/dropdown';
-import {MODAL_DIRECTIVES} from './components/modal';
-import {PAGINATION_DIRECTIVES} from './components/pagination';
-import {PROGRESSBAR_DIRECTIVES} from './components/progressbar';
-import {RatingComponent} from './components/rating';
-import {TAB_DIRECTIVES} from './components/tabs';
-import {TimepickerComponent} from './components/timepicker';
-import {TOOLTIP_DIRECTIVES} from './components/tooltip';
-import {TYPEAHEAD_DIRECTIVES} from './components/typeahead';
+import { ACCORDION_DIRECTIVES } from './components/accordion';
+import { AlertComponent } from './components/alert';
+import { BUTTON_DIRECTIVES } from './components/buttons';
+import { CAROUSEL_DIRECTIVES } from './components/carousel';
+import { CollapseDirective } from './components/collapse';
+import { DATEPICKER_DIRECTIVES } from './components/datepicker';
+import { DROPDOWN_DIRECTIVES } from './components/dropdown';
+import { MODAL_DIRECTIVES } from './components/modal';
+import { PAGINATION_DIRECTIVES } from './components/pagination';
+import { PROGRESSBAR_DIRECTIVES } from './components/progressbar';
+import { RatingComponent } from './components/rating';
+import { TAB_DIRECTIVES } from './components/tabs';
+import { TimepickerComponent } from './components/timepicker';
+import { TOOLTIP_DIRECTIVES } from './components/tooltip';
+import { TYPEAHEAD_DIRECTIVES } from './components/typeahead';
 
-import {ComponentsHelper} from './components/utils/components-helper.service';
+import { ComponentsHelper } from './components/utils/components-helper.service';
 
 export * from './components/accordion';
 export * from './components/alert';
@@ -36,25 +36,25 @@ export * from './components/position';
 export * from './components/common';
 export * from './components/ng2-bootstrap-config';
 
-export {AccordionModule} from './components/accordion/accordion.module';
-export {AlertModule} from './components/alert/alert.module';
-export {ButtonsModule} from './components/buttons/buttons.module';
-export {CarouselModule} from './components/carousel/carousel.module';
-export {CollapseModule} from './components/collapse/collapse.module';
-export {DatepickerModule} from './components/datepicker/datepicker.module';
-export {DropdownModule} from './components/dropdown/dropdown.module';
-export {ModalModule} from './components/modal/modal.module';
-export {PaginationModule} from './components/pagination/pagination.module';
-export {ProgressbarModule} from './components/progressbar/progressbar.module';
-export {RatingModule} from './components/rating/rating.module';
-export {TabsModule} from './components/tabs/tabs.module';
-export {TimepickerModule} from './components/timepicker/timepicker.module';
-export {TooltipModule} from './components/tooltip/tooltip.module';
-export {TypeaheadModule} from './components/typeahead/typeahead.module';
-export {ComponentsHelper} from './components/utils/components-helper.service';
+export { AccordionModule } from './components/accordion/accordion.module';
+export { AlertModule } from './components/alert/alert.module';
+export { ButtonsModule } from './components/buttons/buttons.module';
+export { CarouselModule } from './components/carousel/carousel.module';
+export { CollapseModule } from './components/collapse/collapse.module';
+export { DatepickerModule } from './components/datepicker/datepicker.module';
+export { DropdownModule } from './components/dropdown/dropdown.module';
+export { ModalModule } from './components/modal/modal.module';
+export { PaginationModule } from './components/pagination/pagination.module';
+export { ProgressbarModule } from './components/progressbar/progressbar.module';
+export { RatingModule } from './components/rating/rating.module';
+export { TabsModule } from './components/tabs/tabs.module';
+export { TimepickerModule } from './components/timepicker/timepicker.module';
+export { TooltipModule } from './components/tooltip/tooltip.module';
+export { TypeaheadModule } from './components/typeahead/typeahead.module';
+export { ComponentsHelper } from './components/utils/components-helper.service';
 
 /** @deprecated */
-export const BS_VIEW_PROVIDERS: any[] = [{provide: ComponentsHelper, useClass:ComponentsHelper}];
+export const BS_VIEW_PROVIDERS:any[] = [{provide: ComponentsHelper, useClass: ComponentsHelper}];
 
 /** @deprecated */
 export default {
@@ -80,4 +80,4 @@ export default {
   ]
 };
 
-export {Ng2BootstrapModule} from './components/all';
+export { Ng2BootstrapModule } from './components/index';
