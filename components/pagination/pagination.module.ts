2bedf263d43ed56cb15ac70c35d24e0ec1a3dfbc
mwe <martin.wegner@ebp.de>
2016-09-05 15:33:20 +0200

    try to fix #929 by importing FormsModule because the selectors use [ngModel]

--- components/pagination/pagination.module.ts
+++ components/pagination/pagination.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { PagerComponent } from './pager.component';
 import { PaginationComponent } from './pagination.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   declarations: [PaginationComponent, PagerComponent],
   exports: [PaginationComponent, PagerComponent]
 })
--- components/typeahead/typeahead.module.ts
+++ components/typeahead/typeahead.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { TypeaheadContainerComponent } from './typeahead-container.component';
 import { TypeaheadDirective } from './typeahead.directive';
 import { ComponentsHelper } from '../utils/components-helper.service';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   declarations: [TypeaheadDirective, TypeaheadContainerComponent],
   exports: [TypeaheadDirective, TypeaheadContainerComponent],
   providers: [ComponentsHelper],
