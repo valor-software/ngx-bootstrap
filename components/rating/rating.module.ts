b5c8448d1c5756e97b1b6b3b2b8ebd41cd00412e
Martin-Wegner <martin.wegner@ebp.de>
2016-09-06 16:21:34 +0200

    feat(forms):  add export of FormsModule where [ngModel] selector is present (#931)
    * try to fix #929 by importing FormsModule because the selectors use [ngModel]
    
    * export FormsModule in all modules which use ngModel selector binding because every template which uses such a module would anyway import FormsModul which is now no longer necessary
    
    fixes #929

--- components/rating/rating.module.ts
+++ components/rating/rating.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { RatingComponent } from './rating.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   declarations: [RatingComponent],
-  exports: [RatingComponent]
+  exports: [FormsModule, RatingComponent]
 })
 export class RatingModule {
 }
