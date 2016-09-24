f165c6027a451563275059419e4a86bf8d02978f
Confusedfish <ben@bmcintyre.co.uk>
2016-09-24 13:42:22 +0100

    tslint fix

--- demo/components/tabs/tabs-demo.ts
+++ demo/components/tabs/tabs-demo.ts
@@ -13,7 +13,7 @@ export class TabsDemoComponent {
     {title: 'Dynamic Title 1', content: 'Dynamic content 1'},
     {title: 'Dynamic Title 2', content: 'Dynamic content 2', disabled: true},
     {title: 'Dynamic Title 3', content: 'Dynamic content 3', removable: true},
-    {title: 'Dynamic Title 4', content: 'Dynamic content 4', customClass: "customClass"}
+    {title: 'Dynamic Title 4', content: 'Dynamic content 4', customClass: 'customClass'}
   ];
 
   public alertMe():void {
