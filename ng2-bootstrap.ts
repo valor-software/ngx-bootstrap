3eaa9ad5d2bcac670c48ad6afadb00c67ed285e5
Dmitriy Shekhovtsov <valorkin@gmail.com>
2016-10-05 12:24:35 +0300

    fix(module): class exports added back

--- ng2-bootstrap.ts
+++ ng2-bootstrap.ts
@@ -1,3 +1,21 @@
+export * from './components/accordion';
+export * from './components/alert';
+export * from './components/buttons';
+export * from './components/carousel';
+export * from './components/collapse';
+export * from './components/datepicker';
+export * from './components/modal';
+export * from './components/dropdown';
+export * from './components/pagination';
+export * from './components/progressbar';
+export * from './components/rating';
+export * from './components/tabs';
+export * from './components/timepicker';
+export * from './components/tooltip';
+export * from './components/typeahead';
+
+export * from './components/position';
+export * from './components/common';
 export * from './components/ng2-bootstrap-config';
 
 export { AccordionModule } from './components/accordion/accordion.module';
