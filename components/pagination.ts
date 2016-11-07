1b6a5f61f36deb946487ca765e9971bb37823201
oleg.rybnikov <oleg.rybnikov@valor-software.com>
2016-11-07 16:53:05 +0200

    feat(pagination) export config

--- components/pagination.ts
+++ components/pagination.ts
@@ -1,3 +1,4 @@
 export { PagerComponent } from './pagination/pager.component';
 export { PaginationComponent } from './pagination/pagination.component';
 export { PaginationModule } from './pagination/pagination.module';
+export { PaginationConfig } from './pagination/config';
