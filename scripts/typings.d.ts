212ac3d63535cde908c29e5bc82976ac45c5fa0c
Dmitriy Shekhovtsov <valorkin@gmail.com>
2016-11-10 16:56:20 +0200

    fixing npm start & prepublish & gh-deploy scripts

--- scripts/typings.d.ts
+++ scripts/typings.d.ts
@@ -5,3 +5,5 @@ declare const System: any;
 declare const ENV:string;
 // google code-prettify
 declare const PR:any;
+
+declare const global:any;
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     extensions: ['.ts', '.js']
   },
 
-  entry: helpers.root('src'),
+  entry: helpers.root('src/core'),
 
   output: {
     path: helpers.root(targetFolder),
