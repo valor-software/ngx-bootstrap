2810328465917ef644139f0f7aaf5800da750a0a
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2016-12-15 14:25:20 +0100

    test: remove duplicated imports

--- scripts/test.ts
+++ scripts/test.ts
@@ -6,13 +6,6 @@ import 'zone.js/dist/sync-test';
 import 'zone.js/dist/jasmine-patch';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
-
-import 'zone.js/dist/long-stack-trace-zone';
-import 'zone.js/dist/proxy.js';
-import 'zone.js/dist/sync-test';
-import 'zone.js/dist/jasmine-patch';
-import 'zone.js/dist/async-test';
-import 'zone.js/dist/fake-async-test';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
