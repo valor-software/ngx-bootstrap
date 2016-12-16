b83fd0df64051e7d2752de1489dc8ba330f9632b
Dmitriy Schekhovtsov <valorkin@gmail.com>
2016-12-16 22:48:48 +0200

    fix(config): initial theme set to bs3
    should fix #1307

--- src/utils/ng2-bootstrap-config.ts
+++ src/utils/ng2-bootstrap-config.ts
@@ -3,14 +3,14 @@ import { window } from './facade/browser';
 export enum Ng2BootstrapTheme {BS3 = 1, BS4 = 2}
 
 export class Ng2BootstrapConfig {
-  protected static _theme:Ng2BootstrapTheme;
+  protected static _theme:Ng2BootstrapTheme = Ng2BootstrapTheme.BS3;
 
   public static get theme():Ng2BootstrapTheme {
     // hack as for now
     if (window.__theme === 'bs4') {
       return Ng2BootstrapTheme.BS4;
     }
-    return (this._theme || Ng2BootstrapTheme.BS3);
+    return this._theme;
   }
 
   public static set theme(v:Ng2BootstrapTheme) {
