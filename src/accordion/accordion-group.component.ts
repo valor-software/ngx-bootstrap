53c0401fd1d69eacd211c90eaf5d44d24689d516
Dmitriy Schekhovtsov <valorkin@gmail.com>
2016-12-23 01:12:11 +0200

    fix(accordion): bs4 template updated

--- src/accordion/accordion-group.component.ts
+++ src/accordion/accordion-group.component.ts
@@ -1,11 +1,11 @@
 import {
   Component, HostBinding, Inject, Input, OnDestroy, OnInit
 } from '@angular/core';
-
+import { isBs3 } from '../utils/ng2-bootstrap-config';
 import { AccordionComponent } from './accordion.component';
 
 /*
-* ### Accordion heading
+ * ### Accordion heading
 
  Instead of using `heading` attribute on the `accordion-group`, you can use an `accordion-heading` attribute on `any` element inside of a group that will be used as group's header template.
 
@@ -14,63 +14,67 @@ import { AccordionComponent } from './accordion.component';
 @Component({
   selector: 'accordion-group, accordion-panel',
   template: `
-    <div class="panel" [ngClass]="panelClass">
-      <div class="panel-heading" (click)="toggleOpen($event)">
-        <h4 class="panel-title">
-          <a href tabindex="0" class="accordion-toggle">
-            <span *ngIf="heading" [ngClass]="{'text-muted': isDisabled}">{{heading}}</span>
-            <ng-content select="[accordion-heading]"></ng-content>
-          </a>
-        </h4>
-      </div>
-      <div class="panel-collapse collapse" [collapse]="!isOpen">
-        <div class="panel-body">
-          <ng-content></ng-content>
-        </div>
-      </div>
+<div class="panel card" [ngClass]="panelClass">
+  <div class="panel-heading card-header" role="tab" (click)="toggleOpen($event)">
+    <div class="panel-title card-title">
+      <a href role="button" class="accordion-toggle" [attr.aria-expanded]="isOpen">
+        <div *ngIf="heading"[ngClass]="{'text-muted': isDisabled}">{{heading}}</div>
+        <ng-content select="[accordion-heading]"></ng-content>
+      </a>
     </div>
+  </div>
+  <div class="panel-collapse collapse" role="tabpanel" [collapse]="!isOpen">
+    <div class="panel-body card-block">
+      <ng-content></ng-content>
+    </div>
+  </div>
+</div>
   `
 })
 export class AccordionPanelComponent implements OnInit, OnDestroy {
   /** Clickable text in accordion's group header, check `accordion heading` below for using html in header */
-  @Input() public heading:string;
+  @Input() public heading: string;
   /** Provides an ability to use Bootstrap's contextual panel classes (`panel-primary`, `panel-success`, `panel-info`, etc...). List of all available classes [link](http://getbootstrap.com/components/#panels-alternatives) */
-  @Input() public panelClass:string;
+  @Input() public panelClass: string;
   /** if <code>true</code> — disables accordion group */
-  @Input() public isDisabled:boolean;
+  @Input() public isDisabled: boolean;
 
   // Questionable, maybe .panel-open should be on child div.panel element?
   /** Is accordion group open or closed */
   @HostBinding('class.panel-open')
   @Input()
-  public get isOpen():boolean {
+  public get isOpen(): boolean {
     return this._isOpen;
   }
 
-  public set isOpen(value:boolean) {
+  public set isOpen(value: boolean) {
     this._isOpen = value;
     if (value) {
       this.accordion.closeOtherPanels(this);
     }
   }
 
-  protected _isOpen:boolean;
-  protected accordion:AccordionComponent;
+  public get isBs3(): boolean {
+    return isBs3();
+  }
 
-  public constructor(@Inject(AccordionComponent) accordion:AccordionComponent) {
+  protected _isOpen: boolean;
+  protected accordion: AccordionComponent;
+
+  public constructor(@Inject(AccordionComponent) accordion: AccordionComponent) {
     this.accordion = accordion;
   }
 
-  public ngOnInit():any {
+  public ngOnInit(): any {
     this.panelClass = this.panelClass || 'panel-default';
     this.accordion.addGroup(this);
   }
 
-  public ngOnDestroy():any {
+  public ngOnDestroy(): any {
     this.accordion.removeGroup(this);
   }
 
-  public toggleOpen(event:Event):any {
+  public toggleOpen(event: Event): any {
     event.preventDefault();
     if (!this.isDisabled) {
       this.isOpen = !this.isOpen;
