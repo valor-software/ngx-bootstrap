a693f68de7875ea3f828614b0e3fe636dc6206a5
natali sol <natali.abyss@gmail.com>
2017-01-05 17:12:19 +0200

    docs(pagination): create documentation for pagination

--- demo/src/app/components/pagination/pagination-section.component.ts
+++ demo/src/app/components/pagination/pagination-section.component.ts
@@ -13,41 +13,61 @@ let titleDoc = require('html!markdown!./docs/title.md');
   
     <h2>Contents</h2>
   <ul>
-    <li><a pageScroll href="#usage">Usage</a></li>
-    <li><a pageScroll href="#examples">Examples</a>
+    <li><a routerLink="." fragment="usage">Usage</a></li>
+    <li><a routerLink="." fragment="examples">Examples</a>
       <ul>
-        <!--<li><a pageScroll href="#link-color">Link color</a></li>-->
+        <li><a routerLink="." fragment="default">Default</a></li>
+        <li><a routerLink="." fragment="limit">States & Limits</a></li>
+        <li><a routerLink="." fragment="pager">Pager</a></li>
+        <!--<li><a routerLink="." fragment="styling">Styling</a></li>-->
       </ul>
     </li>
-    <li><a pageScroll href="#api-reference">API Reference</a>
+    <li><a routerLink="." fragment="api-reference">API Reference</a>
       <ul>
-        <li><a pageScroll href="#pagination-component">PaginationComponent</a></li>
-        <li><a pageScroll href="#pager-component">PagerComponent</a></li>
-        <li><a pageScroll href="#pagination-config">PaginationConfig</a></li>
+        <li><a routerLink="." fragment="pagination-component">PaginationComponent</a></li>
+        <li><a routerLink="." fragment="pager-component">PagerComponent</a></li>
+        <li><a routerLink="." fragment="pagination-config">PaginationConfig</a></li>
       </ul>
     </li>
   </ul>   
       
-  <h2 id="usage">Usage</h2>
+  <h2 routerLink="." fragment="usage" id="usage">Usage</h2>
 
   <p [innerHtml]="titleDoc"></p>
 
-  <h2 id="examples">Examples</h2>
+  <h2 routerLink="." fragment="examples" id="examples">Examples</h2>
   
-  <!-- basic -->
-  <ng-sample-box [ts]="demos.old.component" [html]="demos.old.html">
-    <pagination-demo></pagination-demo>
+  <h2 routerLink="." fragment="default" id="default">Default</h2>
+  <ng-sample-box [ts]="demos.basic.component" [html]="demos.basic.html">
+    <demo-pagination-basic></demo-pagination-basic>
+  </ng-sample-box>  
+  
+  <h2 routerLink="." fragment="limit" id="limit">States & Limits</h2>
+  <p>Limit the maximum visible buttons</p>
+  <ng-sample-box [ts]="demos.limit.component" [html]="demos.limit.html">
+    <demo-pagination-limit></demo-pagination-limit>
   </ng-sample-box>
+  
+  <h2 routerLink="." fragment="pager" id="pager">Pager</h2>
+  <ng-sample-box [ts]="demos.pager.component" [html]="demos.pager.html">
+    <demo-pagination-pager></demo-pagination-pager>
+  </ng-sample-box>
+    
+  <!--TODO: temporary disabled pageBtnClass option-->
+  <!--<h2 routerLink="." fragment="styling" id="styling">Styling</h2>-->
+  <!--<ng-sample-box [ts]="demos.styling.component" [html]="demos.styling.html">-->
+    <!--<demo-pagination-styling></demo-pagination-styling>-->
+  <!--</ng-sample-box>-->
       
-  <h2 id="api-reference">API Reference</h2>
-  <ng-api-doc id="pagination-component" directive="PaginationComponent"></ng-api-doc>
-  <ng-api-doc id="pager-component" directive="PagerComponent"></ng-api-doc>
-  <ng-api-doc-config id="pagination-config" type="PaginationConfig"></ng-api-doc-config>
+  <h2 routerLink="." fragment="api-reference" id="api-reference">API Reference</h2>
+  <ng-api-doc routerLink="." fragment="pagination-component" id="pagination-component" directive="PaginationComponent"></ng-api-doc>
+  <ng-api-doc routerLink="." fragment="pager-component" id="pager-component" directive="PagerComponent"></ng-api-doc>
+  <ng-api-doc-config routerLink="." fragment="pagination-config" id="pagination-config" type="PaginationConfig"></ng-api-doc-config>
 </demo-section>`
 })
 export class PaginationSectionComponent {
-  public name:string = 'Pagination';
-  public src:string = 'https://github.com/valor-software/ng2-bootstrap/tree/development/src/pagination';
+  public name: string = 'Pagination';
+  public src: string = 'https://github.com/valor-software/ng2-bootstrap/tree/development/src/pagination';
   public demos: any = DEMOS;
-  public titleDoc:string = titleDoc;
+  public titleDoc: string = titleDoc;
 }
