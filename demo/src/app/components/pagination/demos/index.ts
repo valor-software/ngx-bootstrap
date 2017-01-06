58ca44552e85d6c242a2387665ad2cb1f6b2a00c
Natali Soloviova <natali.abyss@gmail.com>
2017-01-06 15:38:01 +0200

    docs(pagination): create documentation for pagination (#1446)

--- demo/src/app/components/pagination/demos/index.ts
+++ demo/src/app/components/pagination/demos/index.ts
@@ -1,12 +1,27 @@
-import { PaginationDemoComponent } from './pagination-demo.component';
+import { DemoPaginationBasicComponent } from './basic/basic';
+import { DemoPaginationPagerComponent } from './pager/pager';
+import { DemoPaginationLimitComponent } from './limit/limit';
+import { DemoPaginationStylingComponent } from './styling-global/styling-global';
 
 export const DEMO_COMPONENTS = [
-  PaginationDemoComponent
+  DemoPaginationBasicComponent, DemoPaginationPagerComponent, DemoPaginationLimitComponent, DemoPaginationStylingComponent
 ];
 
 export const DEMOS = {
-  old: {
-    component: require('!!raw?lang=typescript!./pagination-demo.component'),
-    html: require('!!raw?lang=markup!./pagination-demo.component.html')
+  basic: {
+    component: require('!!raw?lang=typescript!./basic/basic.ts'),
+    html: require('!!raw?lang=markup!./basic/basic.html')
+  },
+  pager: {
+    component: require('!!raw?lang=typescript!./pager/pager.ts'),
+    html: require('!!raw?lang=markup!./pager/pager.html')
+  },
+  limit: {
+    component: require('!!raw?lang=typescript!./limit/limit.ts'),
+    html: require('!!raw?lang=markup!./limit/limit.html')
+  },
+  styling: {
+    component: require('!!raw?lang=typescript!./styling-global/styling-global.ts'),
+    html: require('!!raw?lang=markup!./styling-global/styling-global.html')
   }
 };
---
+++ demo/src/app/components/pagination/demos/pager/pager.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-pagination-pager',
+  templateUrl: './pager.html'
+})
+export class DemoPaginationPagerComponent {
+  public totalItems:number = 64;
+  public currentPage:number = 4;
+
+  public pageChanged(event:any):void {
+    console.log('Page changed to: ' + event.page);
+    console.log('Number items per page: ' + event.itemsPerPage);
+  }
+}
