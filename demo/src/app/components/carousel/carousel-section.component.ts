44e07ac90b1f151f075dd17d8377a1a036c177e1
Dmitriy Shekhovtsov <valorkin@gmail.com>
2017-01-10 16:46:20 +0200

    chore(docs): updated carousel demo (#1463)
    * feat(docs): upgrade of carousel docs
    
    * chore(demo): changed company logo to github repo link

--- demo/src/app/components/carousel/carousel-section.component.ts
+++ demo/src/app/components/carousel/carousel-section.component.ts
@@ -15,13 +15,16 @@ let titleDoc = require('html!markdown!./docs/title.md');
     <li><a pageScroll href="#usage">Usage</a></li>
     <li><a pageScroll href="#examples">Examples</a>
       <ul>
-        <!--<li><a pageScroll href="#link-color">Link color</a></li>-->
+        <li><a pageScroll href="#captions">Optional captions</a></li>
+        <li><a pageScroll href="#config">Configuring defaults</a></li>
+        <li><a pageScroll href="#dynamic">Dynamic Slides</a></li>
       </ul>
     </li>
     <li><a pageScroll href="#api-reference">API Reference</a>
       <ul>
         <li><a pageScroll href="#carousel-component">CarouselComponent</a></li>
         <li><a pageScroll href="#slide-component">SlideComponent</a></li>
+        <li><a pageScroll href="#carousel-config">CarouselConfig</a></li>
       </ul>
     </li>
   </ul>   
@@ -33,19 +36,40 @@ let titleDoc = require('html!markdown!./docs/title.md');
   <h2 id="examples">Examples</h2>
    
   <!-- basic -->
-  <ng-sample-box [ts]="demos.old.component" [html]="demos.old.html">
-    <carousel-demo></carousel-demo>
+  <ng-sample-box [ts]="demos.basic.component" [html]="demos.basic.html">
+    <demo-carousel-basic></demo-carousel-basic>
+  </ng-sample-box>
+    
+  <!-- captions -->
+  <h2 id="captions">Optional captions</h2>
+  <p>Add captions to your slides easily with the <code>.carousel-caption</code> element within any <code>&lt;slide></code>.
+ Place just about any optional HTML within there and it will be automatically aligned and formatted.</p>
+  <ng-sample-box [ts]="demos.captions.component" [html]="demos.captions.html">
+    <demo-carousel-captions></demo-carousel-captions>
+  </ng-sample-box>
+    
+  <!-- config -->
+  <h2 id="config">Configuring defaults</h2>
+  <ng-sample-box [ts]="demos.config.component" [html]="demos.config.html">
+    <demo-carousel-config></demo-carousel-config>
+  </ng-sample-box>
+  
+  <!-- dynamic -->
+  <h2 id="dynamic">Dynamic Slides</h2>
+  <ng-sample-box [ts]="demos.dynamic.component" [html]="demos.dynamic.html">
+    <demo-carousel-dynamic></demo-carousel-dynamic>
   </ng-sample-box>
   
   <h2 id="api-reference">API Reference</h2>
   <ng-api-doc id="carousel-component" directive="CarouselComponent"></ng-api-doc>
   <ng-api-doc id="slide-component" directive="SlideComponent"></ng-api-doc>
+  <ng-api-doc-config id="carousel-config" type="CarouselConfig"></ng-api-doc-config>
 </demo-section>
 `
 })
 export class CarouselSectionComponent {
-  public name:string = 'Carousel';
-  public src:string = 'https://github.com/valor-software/ng2-bootstrap/tree/development/src/carousel';
+  public name: string = 'Carousel';
+  public src: string = 'https://github.com/valor-software/ng2-bootstrap/tree/development/src/carousel';
   public demos: any = DEMOS;
-  public titleDoc:string = titleDoc;
+  public titleDoc: string = titleDoc;
 }
