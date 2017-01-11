35d8f99bd23e9c486e3982dee01b4d394e8d9e11
Natali Soloviova <natali.abyss@gmail.com>
2017-01-11 13:24:35 +0200

    docs(tabs): create documentation for tabs (#1470)

---
+++ demo/src/app/components/tabs/demos/vertical-pills/vertical-pills.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-tabs-vertical-pills',
+  templateUrl: './vertical-pills.html'
+})
+export class DemoTabsVerticalPillsComponent {
+
+}
