ab3d3dcf4a10c51053f4ff540417f36443f1ab93
natali sol <natali.abyss@gmail.com>
2017-01-11 17:45:54 +0200

    docs(timepicker): create documentation for timepicker

--- demo/src/app/components/timepicker/demos/index.ts
+++ demo/src/app/components/timepicker/demos/index.ts
@@ -1,12 +1,38 @@
-import { TimepickerDemoComponent } from './timepicker-demo.component';
+import { DemoTimepickerBasicComponent } from './basic/basic';
+import { DemoTimepickerConfigComponent } from './config/config';
+import { DemoTimepickerMeridianComponent } from './meridian/meridian';
+import { DemoTimepickerDisabledComponent } from './disabled/disabled';
+import { DemoTimepickerCustomComponent } from './custom/custom';
+import { DemoTimepickerDynamicComponent } from './dynamic/dynamic';
 
 export const DEMO_COMPONENTS = [
-  TimepickerDemoComponent
+  DemoTimepickerBasicComponent, DemoTimepickerConfigComponent, DemoTimepickerMeridianComponent,
+  DemoTimepickerDisabledComponent, DemoTimepickerCustomComponent, DemoTimepickerDynamicComponent
 ];
 
 export const DEMOS = {
-  old: {
-    component: require('!!raw?lang=typescript!./timepicker-demo.component'),
-    html: require('!!raw?lang=markup!./timepicker-demo.component.html')
+  basic: {
+    component: require('!!raw?lang=typescript!./basic/basic'),
+    html: require('!!raw?lang=markup!./basic/basic.html')
+  },
+  meridian: {
+    component: require('!!raw?lang=typescript!./meridian/meridian'),
+    html: require('!!raw?lang=markup!./meridian/meridian.html')
+  },
+  disabled: {
+    component: require('!!raw?lang=typescript!./disabled/disabled'),
+    html: require('!!raw?lang=markup!./disabled/disabled.html')
+  },
+  custom: {
+    component: require('!!raw?lang=typescript!./custom/custom'),
+    html: require('!!raw?lang=markup!./custom/custom.html')
+  },
+  dynamic: {
+    component: require('!!raw?lang=typescript!./dynamic/dynamic'),
+    html: require('!!raw?lang=markup!./dynamic/dynamic.html')
+  },
+  config: {
+    component: require('!!raw?lang=typescript!./config/config'),
+    html: require('!!raw?lang=markup!./config/config.html')
   }
 };
