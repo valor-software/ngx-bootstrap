40ae93abb51633b380f69752abad9365082fe93b
Le0Michine <leomichine@gmail.com>
2017-01-11 16:15:31 +0100

    moved styles into html

--- demo/src/app/components/sortable/demos/simple-items/simple-items.component.ts
+++ demo/src/app/components/sortable/demos/simple-items/simple-items.component.ts
@@ -16,29 +16,4 @@ export class SimpleItemsDemoComponent {
     'Mr. O',
     'Tomato'
   ];
-
-  public itemStyle: {} = {
-    display: 'block',
-    padding: '6px 12px',
-    'margin-bottom': '4px',
-    'font-size': '14px',
-    'font-weight': 400,
-    'line-height': '1.4em',
-    'text-align': 'center',
-    cursor: 'grab',
-    border: '1px solid transparent',
-    'border-radius': '4px',
-    'border-color': '#adadad'
-  };
-
-  public itemActiveStyle: {} = {
-    'background-color': '#e6e6e6',
-    'box-shadow': 'inset 0 3px 5px rgba(0,0,0,.125)'
-  };
-
-  public wrapperStyle: {} = {
-    'min-height': '150px'
-  };
-
-  public placeholderStyle: {} = Object.assign({}, this.itemStyle, { height: '150px' });
 }
