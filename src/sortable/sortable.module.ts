fbd402aa74ad829ff72e51469791a9640d21a718
Somo Makombo <somombo@users.noreply.github.com>
2017-01-15 23:35:26 -0600

    changed code styling to be consistent with the rest
    Hopefully it will pass CI tests now!

--- src/sortable/sortable.module.ts
+++ src/sortable/sortable.module.ts
@@ -1,5 +1,5 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
-import {CommonModule} from "@angular/common";
+import { CommonModule } from '@angular/common';
 
 import { SortableComponent } from './sortable.component';
 import { DraggableItemService } from './draggable-item.service';
