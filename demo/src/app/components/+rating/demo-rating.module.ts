a327db390d0aa8f2c5fcd54048e69fd2ec90663a
Dmitriy Shekhovtsov <valorkin@gmail.com>
2017-02-03 16:55:23 +0200

    chore(demo): lazy loading for all components

---
+++ demo/src/app/components/+rating/demo-rating.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { RatingModule } from 'ng2-bootstrap/rating';
+
+import { SharedModule } from '../../shared';
+import { RatingSectionComponent } from './rating-section.component';
+import { DEMO_COMPONENTS } from './demos';
+import { routes } from './demo-rating.routes';
+
+@NgModule({
+  declarations: [
+    RatingSectionComponent,
+    ...DEMO_COMPONENTS
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    SharedModule,
+    RatingModule.forRoot(),
+    RouterModule.forChild(routes)
+  ],
+  exports: [RatingSectionComponent]
+})
+export class DemoRatingModule {
+
+}
