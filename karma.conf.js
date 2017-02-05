0373874ce8fbfa0f32b9eda131d5f2b6b488cf20
Leo <leomichine@gmail.com>
2017-02-05 20:20:17 +0100

    fixed karma config

--- karma.conf.js
+++ karma.conf.js
@@ -6,18 +6,18 @@ const customLaunchers = require('./scripts/sauce-browsers').customLaunchers;
 module.exports = function (config) {
   const configuration = {
     basePath: '',
-    frameworks: ['jasmine', 'angular-cli'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-remap-istanbul'),
-      require('angular-cli/plugins/karma')
+      require('@angular/cli/plugins/karma')
     ],
     files: [
       {pattern: './scripts/test.ts', watched: false}
     ],
     preprocessors: {
-      './scripts/test.ts': ['angular-cli']
+      './scripts/test.ts': ['@angular/cli']
     },
     remapIstanbulReporter: {
       reports: {
