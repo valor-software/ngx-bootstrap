63c64e0c07bfc8fbea566367aa697124afd46684
Rafał Kasa <rafalkasa@gmail.com>
2017-02-28 22:26:47 +0100

    feat(demo): access static tabs sample (#1603)

--- demo/src/app/components/+tabs/demos/basic/basic.ts
+++ demo/src/app/components/+tabs/demos/basic/basic.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { TabsetComponent } from 'ng2-bootstrap';
 
 @Component({
   selector: 'demo-tabs-basic',
@@ -6,9 +7,19 @@ import { Component } from '@angular/core';
 })
 export class DemoTabsBasicComponent {
 
+  @ViewChild('staticTabs') staticTabs: TabsetComponent;
+
   public alertMe(): void {
     setTimeout(function (): void {
       alert('You\'ve selected the alert tab!');
     });
   }
+
+  selectTab(tab_id: number) {
+      this.staticTabs.tabs[tab_id].active = true;
+  }
+
+  disableEnable() {
+    this.staticTabs.tabs[2].disabled = ! this.staticTabs.tabs[2].disabled
+  }  
 }
