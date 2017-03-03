788ab4f262b3b7f5f1653b195d50b3696c046100
Dmitriy Shekhovtsov <valorkin@gmail.com>
2017-03-03 18:29:20 +0200

    simplifying

--- src/sortable/bs-sortable.component.ts
+++ src/sortable/bs-sortable.component.ts
@@ -1,51 +1,41 @@
 import {
-  Component, Input, Output, EventEmitter, forwardRef, ChangeDetectorRef, TemplateRef
+  Component, Input, Output, EventEmitter, forwardRef, ChangeDetectorRef, TemplateRef, OnInit
 } from '@angular/core';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
+// placeholderItem - could be an element, not just text
 
 @Component({
   selector: 'bs-sortable',
   exportAs: 'bs-sortable',
   template: `
-  <div
-    [bsDropZone]="currentDropZoneId"
-    [dropZoneGroup]="dropZoneGroup"
-    [acceptFromZones]="acceptFromZones"
-    [fixDisabledItems]="fixDisabledItems"
-    [(items)]="items"
-    (activeItemIndexChange)="onActiveItemChange($event)"
-    [ngClass]="wrapperClass"
-    [ngStyle]="wrapperStyle"
-    [ngStyle]="wrapperStyle"
-    [ghostClassName]="ghostClassName"
-  >
     <div
-      *ngIf="showPlaceholder"
-      [ngClass]="placeholderClass"
-      [ngStyle]="placeholderStyle"
-    >{{placeholderItem}}</div>
-    <div
-      bsDraggable
-      *ngFor="let item of items; let i = index;"
-      [disabled]="isCurrentItemDisabled(item)"
-      [ngClass]="[ itemClass, isItemActive(i) ? itemActiveClass : '', isCurrentItemDisabled(item) ? itemDisabledClass : '' ]"
-      [ngStyle]="getItemStyle(isItemActive(i))"
-    >
-      <template
-        [ngTemplateOutlet]="itemTemplate || defItemTemplate"
-        [ngOutletContext]="{item: item, index: i}"
-      ></template>
+      [bsDropZone]="currentDropZoneId"
+      [dropZoneGroup]="dropZoneGroup"
+      [acceptFromZones]="acceptFromZones"
+      [fixDisabledItems]="fixDisabledItems"
+      [(items)]="items"
+      (activeItemIndexChange)="onActiveItemChange($event)"
+      [ngClass]="wrapperClass"
+      [ghostClassName]="ghostClassName">
+      <div *ngIf="isEmpty" class="bs-sortable-placeholder">{{placeholderItem}}</div>
+      <div bsDraggable class="bs-sortable-item"
+           *ngFor="let item of items; let i = index;"
+           [class.bs-active]="index === activeItem"
+           [attr.disabled]="isCurrentItemDisabled(item)">
+        <template [ngTemplateOutlet]="itemTemplate || defItemTemplate" [ngOutletContext]="{item: item, index: i}">
+        </template>
+      </div>
     </div>
-  </div>
 
-  <template #defItemTemplate let-item="item">{{ fieldName ? item[fieldName] : item }}</template>`,
+    <template #defItemTemplate let-item="item">{{ fieldName ? item[fieldName] : item }}</template>`,
   providers: [{
     provide: NG_VALUE_ACCESSOR,
     useExisting: forwardRef(() => BsSortableComponent),
     multi: true
   }]
 })
-export class BsSortableComponent implements ControlValueAccessor {
+export class BsSortableComponent implements ControlValueAccessor, OnInit {
   private static globalZoneIndex: number = 0;
 
   /** name of sortable, should be unique for all the Sortable components and dropZone directives on the page */
@@ -69,32 +59,8 @@ export class BsSortableComponent implements ControlValueAccessor {
   /** class name for items wrapper */
   @Input() public wrapperClass: string = '';
 
-  /** style object for items wrapper */
-  @Input() public wrapperStyle: {[key: string]: string} = {};
-
-  /** class name for item */
-  @Input() public itemClass: string = '';
-
-  /** style object for item */
-  @Input() public itemStyle: {[key: string]: string} = {};
-
-  /** class name for active item */
-  @Input() public itemActiveClass: string = '';
-
-  /** style object for active item */
-  @Input() public itemActiveStyle: {[key: string]: string} = {};
-
-  /** class name for disabled item */
-  @Input() public itemDisabledClass: string = '';
-
   /** class name for ghost item (applicable for touch only) */
   @Input() public ghostClassName: string = '';
-
-  /** class name for placeholder */
-  @Input() public placeholderClass: string = '';
-
-  /** style object for placeholder */
-  @Input() public placeholderStyle: {[key: string]: string} = {};
 
   /** placeholder item which will be shown if collection is empty */
   @Input() public placeholderItem: string = '';
@@ -110,11 +76,11 @@ export class BsSortableComponent implements ControlValueAccessor {
   public showPlaceholder: boolean = false;
   public activeItem: number = -1;
 
+  // ControlValueAccessor
   public onTouched: any = Function.prototype;
   public onChanged: any = Function.prototype;
 
   private _items: any[];
-  private defaultZoneIndex: string;
 
   public get items(): any[] {
     return this._items;
@@ -124,16 +90,25 @@ export class BsSortableComponent implements ControlValueAccessor {
     this._items = value;
     this.onChanged(value);
     this.onChange.emit(value);
-    this.updatePlaceholderState();
   }
 
-  public get currentDropZoneId(): string {
-    if (!this.dropZoneId && !this.defaultZoneIndex) {
-      this.defaultZoneIndex = `sortableZone${BsSortableComponent.globalZoneIndex++}`;
-    }
-    return this.dropZoneId || this.defaultZoneIndex;
+  public ngOnInit(): void {
+    this.dropZoneId = this.dropZoneId || `bs-sortable-zone-id-${BsSortableComponent.globalZoneIndex++}`;
   }
 
+  public get isEmpty(): void {
+    return !this._items || !this._items.length;
+  }
+
+  public onActiveItemChange(index: number): void {
+    this.activeItem = index;
+  }
+
+  public isCurrentItemDisabled(item: any): boolean {
+    return this.isItemDisabled && this.isItemDisabled(item);
+  }
+
+  // ControlValueAccessor
   public registerOnChange(callback: (_: any) => void): void {
     this.onChanged = callback;
   }
@@ -144,27 +119,5 @@ export class BsSortableComponent implements ControlValueAccessor {
 
   public writeValue(value: any[]): void {
     this.items = value || [];
-  }
-
-  public updatePlaceholderState(): void {
-    this.showPlaceholder = !this._items.length;
-  }
-
-  public onActiveItemChange(index: number): void {
-    this.activeItem = index;
-  }
-
-  public getItemStyle(isActive: boolean): {} {
-    return isActive
-      ? Object.assign({}, this.itemStyle, this.itemActiveStyle)
-      : this.itemStyle;
-  }
-
-  public isItemActive(index: number): boolean {
-    return index === this.activeItem;
-  }
-
-  public isCurrentItemDisabled(item: any): boolean {
-    return this.isItemDisabled && this.isItemDisabled(item);
   }
 }
