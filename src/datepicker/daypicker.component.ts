fc323ccb3ff3a7fad7881326a88a6d8234253005
Dmitriy Schekhovtsov <valorkin@gmail.com>
2017-03-30 23:56:08 +0300

    feat(package): dropped support of ng v2

--- src/datepicker/daypicker.component.ts
+++ src/datepicker/daypicker.component.ts
@@ -58,7 +58,7 @@ const TEMPLATE_OPTIONS: any = {
     </tr>
   </thead>
   <tbody>
-    <template ngFor [ngForOf]="rows" let-rowz="$implicit" let-index="index">
+    <ng-template ngFor [ngForOf]="rows" let-rowz="$implicit" let-index="index">
       <tr *ngIf="!(datePicker.onlyCurrentMonth && rowz[0].secondary && rowz[6].secondary)">
         <td *ngIf="datePicker.showWeeks" class="h6" class="text-center">
           <em>{{ weekNumbers[index] }}</em>
@@ -73,7 +73,7 @@ const TEMPLATE_OPTIONS: any = {
           </button>
         </td>
       </tr>
-    </template>
+    </ng-template>
   </tbody>
 </table>
   `
