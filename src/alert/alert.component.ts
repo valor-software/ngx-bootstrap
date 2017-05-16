8dfcec9d857cb00102ce5fa892f44f94d9ca17bd
Gustavo Américo <gustavo.americo@live.com>
2017-05-16 14:51:34 -0300

    Atualização da diretiva do angular 4 ng-template

--- src/alert/alert.component.ts
+++ src/alert/alert.component.ts
@@ -5,17 +5,17 @@ import { OnChange } from '../utils/decorators';
 @Component({
   selector: 'alert,ngx-alert',
   template: `
-<template [ngIf]="!isClosed">
+<ng-template [ngIf]="!isClosed">
   <div [class]="'alert alert-' + type" role="alert" [ngClass]="classes">
-    <template [ngIf]="dismissible">
+    <ng-template [ngIf]="dismissible">
       <button type="button" class="close" aria-label="Close" (click)="close()">
         <span aria-hidden="true">&times;</span>
         <span class="sr-only">Close</span>
       </button>
-    </template>
+    </ng-template>
     <ng-content></ng-content>
   </div>
-</template>
+</ng-template>
   `
 })
 export class AlertComponent implements OnInit {
