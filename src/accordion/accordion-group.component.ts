ba56e64478caa8f16bb85b95a43fedf56d6a9bd2
tibs <7185@users.noreply.github.com>
2017-06-02 15:50:28 +0200

    fix(accoridon): remove unneeded card-title class (#2024)

--- src/accordion/accordion-group.component.ts
+++ src/accordion/accordion-group.component.ts
@@ -16,7 +16,7 @@ import { AccordionComponent } from './accordion.component';
   template: `
 <div class="panel card" [ngClass]="panelClass">
   <div class="panel-heading card-header" role="tab" (click)="toggleOpen($event)">
-    <div class="panel-title card-title">
+    <div class="panel-title">
       <div role="button" class="accordion-toggle" [attr.aria-expanded]="isOpen">
         <div *ngIf="heading"[ngClass]="{'text-muted': isDisabled}">{{heading}}</div>
         <ng-content select="[accordion-heading]"></ng-content>
