62ae2c490096adc6ba0cbfb4c46c1a0cbc5cf530
Ilya Surmay <ilya.surmay@valor-software.com>
2017-06-14 14:08:13 +0300

    fix(modal): fix service path

---
+++ demo/src/app/components/+modal/demos/service/service.ts
@@ -0,0 +1,21 @@
+import { Component, ElementRef, Renderer, TemplateRef, ViewContainerRef } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { ModalContainerComponent } from 'ngx-bootstrap/modal';
+import { ModalContentComponent } from './modal-content.component';
+
+@Component({
+  selector: 'demo-modal-service',
+  templateUrl: './service.html'
+})
+export class DemoModalServiceComponent {
+  public modalRef: ModalContainerComponent;
+  constructor(private modalService: BsModalService, private element: ElementRef, private renderer: Renderer, private vcRef: ViewContainerRef) {
+    this.modalService = this.modalService.create(this.element, this.vcRef, this.renderer);
+  }
+  public openModal(template: string | TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, {backdrop: false});
+  }
+  public openModalWithComponent() {
+    this.modalService.show(ModalContentComponent);
+  }
+}
