dddede098b5cd4f5732d050e675cca5628d5dee0
Dmitriy Shekhovtsov <valorkin@gmail.com>
2017-06-26 17:40:01 +0300

    chore(modals): simplify service

--- src/modal/bs-modal.service.ts
+++ src/modal/bs-modal.service.ts
@@ -2,104 +2,90 @@ import { ComponentRef, ElementRef, Injectable, Renderer, TemplateRef, ViewContai
 
 import { ComponentLoader } from '../component-loader/component-loader.class';
 import { ComponentLoaderFactory } from '../component-loader/component-loader.factory';
-import { Utils } from '../utils/utils.class';
 import { ModalBackdropComponent } from './modal-backdrop.component';
 import { ModalContainerComponent } from './modal-container.component';
 import { BsModalRef, modalConfigDefaults, ModalOptions } from './modal-options.class';
 
-const TRANSITION_DURATION = 300;
 const BACKDROP_TRANSITION_DURATION = 150;
 
 @Injectable()
 export class BsModalService {
   // constructor props
-  public isAnimated: boolean = true;
+  public isAnimated = true;
   public config: ModalOptions = modalConfigDefaults;
-  protected _isShown: boolean = false;
-  protected timerRmBackDrop: number = 0;
+
   protected backdropRef: ComponentRef<ModalBackdropComponent>;
-  private _backdrop: ComponentLoader<ModalBackdropComponent>;
-  private _modal: ComponentLoader<ModalContainerComponent>;
+
+  private _backdropLoader: ComponentLoader<ModalBackdropComponent>;
+  private _modalLoader: ComponentLoader<ModalContainerComponent>;
 
   public constructor(private clf: ComponentLoaderFactory) {}
 
   /** Initialization of BsModalService, requires ElementRef, ViewContainerRef and Renderer instances */
-  public create(_element: ElementRef, _viewContainerRef: ViewContainerRef, _renderer: Renderer) {
-    this._backdrop = this.clf.createLoader<ModalBackdropComponent>(_element, _viewContainerRef, _renderer);
-    this._modal = this.clf
+  create(_element: ElementRef, _viewContainerRef: ViewContainerRef, _renderer: Renderer) {
+    this._backdropLoader = this.clf.createLoader<ModalBackdropComponent>(_element, _viewContainerRef, _renderer);
+    this._modalLoader = this.clf
       .createLoader<ModalContainerComponent>(_element, _viewContainerRef, _renderer);
     return this;
   }
 
   /** Shows a modal */
-  public show(content: string | TemplateRef<any> | any, config: any = {}): BsModalRef {
+  show(content: string | TemplateRef<any> | any, config?: any): BsModalRef {
     this.config = Object.assign({}, modalConfigDefaults, config);
-    clearTimeout(this.timerRmBackDrop);
-    this._isShown = true;
-    this.toggleBackdrop();
+
+    this._showBackdrop();
+    return this._showModal(content);
+  }
+
+  hide() {
+    this._hideBackdrop();
+    setTimeout(() => this._hideModal(), BACKDROP_TRANSITION_DURATION);
+  }
+
+  _showBackdrop(): void {
+    const isBackdropEnabled = this.config.backdrop || this.config.backdrop === 'static';
+    const isBackdropInDOM = !this.backdropRef || !this.backdropRef.instance.isShown;
+
+    this.removeBackdrop();
+
+    if (isBackdropEnabled && isBackdropInDOM) {
+      this._backdropLoader
+        .attach(ModalBackdropComponent)
+        .to('body')
+        .show({isAnimated: this.isAnimated});
+      this.backdropRef = this._backdropLoader._componentRef;
+    }
+  }
+
+  _hideBackdrop(): void {
+    if (!this.backdropRef) {
+      return;
+    }
+    this.backdropRef.instance.isShown = false;
+    const duration = this.isAnimated ? BACKDROP_TRANSITION_DURATION : 0;
+    setTimeout(() => this.removeBackdrop(), duration);
+  }
+
+  _showModal(content: any): BsModalRef {
     const bsModalRef = new BsModalRef();
-    const modalContainerRef = this._modal
+    const modalContainerRef = this._modalLoader
       .provide({provide: ModalOptions, useValue: this.config})
       .provide({provide: BsModalRef, useValue: bsModalRef})
       .attach(ModalContainerComponent)
       .show({content});
     bsModalRef.hide = () => {modalContainerRef.instance.hide();};
-    bsModalRef.content = this._modal.getInnerComponent() || null;
+    bsModalRef.content = this._modalLoader.getInnerComponent() || null;
     return bsModalRef;
   }
 
-  public hide() {
-    this._isShown = false;
-    if (this.backdropRef && this.backdropRef.instance) {
-      this.backdropRef.instance.isShown = false;
+  _hideModal(): void {
+    if (this._modalLoader) {
+      this._modalLoader.hide();
     }
-    setTimeout(() => {
-      this.removeBackdrop();
-      if (this._modal) {
-        this._modal.hide();
-      }
-      this.backdropRef = null;
-    }, BACKDROP_TRANSITION_DURATION);
-  }
-
-  /** @internal */
-  protected toggleBackdrop(): void {
-    // this if will not happen
-    if (!this._isShown && this.backdropRef) {
-      return this._hideBackdrop();
-    }
-
-    const isBackdropEnabled = this.config.backdrop || this.config.backdrop === 'static';
-    const isBackdropInDOM = !this.backdropRef || !this.backdropRef.instance.isShown;
-    if (this._isShown && isBackdropEnabled && isBackdropInDOM) {
-      return this._showBackdrop();
-    }
-  }
-
-  _showBackdrop(): void {
-    this.removeBackdrop();
-    this._backdrop
-      .attach(ModalBackdropComponent)
-      .to('body')
-      // .show({isAnimated: false});
-      .show({isAnimated: this.isAnimated});
-    this.backdropRef = this._backdrop._componentRef;
-
-    // if (this.isAnimated) {
-    //   this.backdropRef.instance.isAnimated = this.isAnimated;
-    //   Utils.reflow(this.backdropRef.instance.element.nativeElement);
-    // }
-    //
-    // this.backdropRef.instance.isShown = true;
-  }
-
-  _hideBackdrop(): void {
-    this.backdropRef.instance.isShown = false;
-    const duration = this.backdropRef.instance.isAnimated ? BACKDROP_TRANSITION_DURATION: 0;
-    this.timerRmBackDrop = setTimeout(() => this.removeBackdrop(), duration);
   }
 
   protected removeBackdrop(): void {
-    this._backdrop.hide();
+    this._backdropLoader.hide();
+    this.backdropRef = null;
   }
 }
