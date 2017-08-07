d18ef97b27109fffca3a8e5cfd88279f216a7cd8
Vyacheslav Panchenko <vyacheslav.panchenko@valor-software.com>
2017-08-07 14:45:35 +0300

    fix(weeks): change calculation of week number

--- src/datepicker2/day/bs-day-picker.component.ts
+++ src/datepicker2/day/bs-day-picker.component.ts
@@ -10,41 +10,42 @@ import { BsCalendarOptionsClass } from '../common/bs-calendar-options.provider';
   selector: 'bs-day-picker',
   exportAs: 'bs-day-picker',
   template: `
-<div ></div>
-<div class="bs-datepicker-head label-success">
-  <button class="previous" (click)="viewPrev('months')"><span>&lsaquo;</span></button>
-  <button class="current" (click)="viewMode('months')"><span>{{viewMonth}}</span></button>
-  <button class="current" (click)="viewMode('years')"><span>{{viewYear}}</span></button>
-  <button class="next" (click)="viewNext('months')"><span>&rsaquo;</span></button>
-</div>
-<div class="bs-datepicker-body">
+    <div></div>
+    <div class="bs-datepicker-head label-success">
+      <button class="previous" (click)="viewPrev('months')"><span>&lsaquo;</span></button>
+      <button class="current" (click)="viewMode('months')"><span>{{viewMonth}}</span></button>
+      <button class="current" (click)="viewMode('years')"><span>{{viewYear}}</span></button>
+      <button class="next" (click)="viewNext('months')"><span>&rsaquo;</span></button>
+    </div>
+    <div class="bs-datepicker-body">
 
-  <table role="grid" class="days weeks">
-    <thead>
-    <tr>
-      <th *ngIf="options.ui.showWeekNumbers"></th><!--if show weeks-->
-      <th *ngFor="let weekday of locale.weekdays; let i = index" aria-label="weekday">{{locale.weekdaysShort[i]}}</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr *ngFor="let week of calendar; let i = index">
-      <td class="week" *ngIf="options.ui.showWeekNumbers"><span>{{ weeks[i] }}</span></td>
-      <td *ngFor="let day of week" role="gridcell"
-          (click)="selectDate(day.date)" (mouseenter)="activeDate(day.date)" (mouseleave)="activeDate()"
-          [class.disabled]="day.isDisabled"
-          [class.is-highlighted]="day.isHighlighted"
-          [class.is-other-month]="day.isOtherMonth"
-          [class.active]="day.isActive"
-          [class.select-start]="day.isSelectionStart"
-          [class.select-end]="day.isSelectionEnd"
-          [class.selected]="day.isSelected">
-        <span>{{day.label}}</span>
-      </td>
-    </tr>
-    </tbody>
-  </table>
-</div>
-`,
+      <table role="grid" class="days weeks">
+        <thead>
+        <tr>
+          <th *ngIf="options.ui.showWeekNumbers"></th><!--if show weeks-->
+          <th *ngFor="let weekday of locale.weekdays; let i = index" aria-label="weekday">{{locale.weekdaysShort[i]}}
+          </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr *ngFor="let week of calendar; let i = index">
+          <td class="week" *ngIf="options.ui.showWeekNumbers"><span>{{ weeks[i] }}</span></td>
+          <td *ngFor="let day of week" role="gridcell"
+              (click)="selectDate(day.date)" (mouseenter)="activeDate(day.date)" (mouseleave)="activeDate()"
+              [class.disabled]="day.isDisabled"
+              [class.is-highlighted]="day.isHighlighted"
+              [class.is-other-month]="day.isOtherMonth"
+              [class.active]="day.isActive"
+              [class.select-start]="day.isSelectionStart"
+              [class.select-end]="day.isSelectionEnd"
+              [class.selected]="day.isSelected">
+            <span>{{day.label}}</span>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BsDayPickerComponent extends DatePickerBase {
@@ -82,7 +83,7 @@ export class BsDayPickerComponent extends DatePickerBase {
     }
 
     this.calendar = this.getDaysCalendarMatrix(viewDate);
-    this.weeks = this.getWeeksNumbers(viewDate);
+    this.weeks = this.getWeeksNumbers(this.calendar);
     this.locale = this.getLocale();
     this.viewMonth = moment(viewDate).format(this.options.format.monthTitle);
     this.viewYear = moment(viewDate).format(this.options.format.yearTitle);
