b1a43f3f1cc4c00705b90566cb3197b8342369b8
Andreas Bade <andreas.bade@identpro.de>
2017-08-17 12:06:36 +0200

    added on drop event docs

--- demo/src/app/components/+sortable/demos/index.ts
+++ demo/src/app/components/+sortable/demos/index.ts
@@ -1,11 +1,13 @@
 import { ComplexDatamodelDemoComponent } from './complex-datamodel/complex-datamodel.component';
 import { SimpleItemsDemoComponent } from './simple-items/simple-items.component';
 import { CustomItemTemplateDemoComponent } from './custom-item-template/custom-item-template';
+import { OnDropEventComponent } from './on-drop-event/on-drop-event.component';
 
 export const DEMO_COMPONENTS = [
   SimpleItemsDemoComponent,
   ComplexDatamodelDemoComponent,
-  CustomItemTemplateDemoComponent
+  CustomItemTemplateDemoComponent,
+  OnDropEventComponent
 ];
 
 export const DEMOS = {
@@ -20,5 +22,9 @@ export const DEMOS = {
   itemTemplate: {
     component: require('!!raw-loader?lang=typescript!./custom-item-template/custom-item-template.ts'),
     html: require('!!raw-loader?lang=markup!./custom-item-template/custom-item-template.html')
+  },
+  onDropEvent: {
+    component: require('!!raw-loader?lang=typescript!./on-drop-event/on-drop-event.component.ts'),
+    html: require('!!raw-loader?lang=markup!./on-drop-event/on-drop-event.component.html')
   }
 };
