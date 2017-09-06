5e7d2a7ff8463ce835909e97fca0be89c57f63b3
Ilya Surmay <ilya.surmay@valor-software.com>
2017-09-06 17:01:33 +0300

    demo(datepicker): remove duplicate of disabled demo

--- demo/src/app/components/+datepicker/demos/is-disabled/disabled-datepicker.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'demo-disabled-datepicker',
-  templateUrl: './disabled-datepicker.html'
-})
-export class DemoDisabledDatepickerComponent {
-  isDisabled = true;
-}
