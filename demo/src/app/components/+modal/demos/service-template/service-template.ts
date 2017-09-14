13043fc8dbadabb63226cafe3e089cf78cc2a5a7
Ilya Surmay <ilya.surmay@gmail.com>
2017-09-14 13:42:47 +0300

    fix(modal): fix bsModalRef paths in demos (#2638)

--- demo/src/app/components/+modal/demos/service-template/service-template.ts
+++ demo/src/app/components/+modal/demos/service-template/service-template.ts
@@ -1,6 +1,6 @@
 import { Component, TemplateRef } from '@angular/core';
 import { BsModalService } from 'ngx-bootstrap/modal';
-import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
 @Component({
   selector: 'demo-modal-service-static',
