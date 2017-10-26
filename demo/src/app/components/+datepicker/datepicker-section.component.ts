1e22198185310d4243d5f6828ddeb3805dca8d9c
Dmitriy Shekhovtsov <valorkin@gmail.com>
2017-10-26 15:09:46 +0300

    chore(demo): unified demo section component

--- demo/src/app/components/+datepicker/datepicker-section.component.ts
+++ demo/src/app/components/+datepicker/datepicker-section.component.ts
@@ -7,44 +7,28 @@ import { ContentSection } from '../../shared/models/content-section.model';
 @Component({
   selector: 'datepicker-section',
   templateUrl: './datepicker-section.component.html',
-  changeDetection: ChangeDetectionStrategy.Default
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DatepickerSectionComponent {
   name = 'Datepicker';
   src = 'https://github.com/valor-software/ngx-bootstrap/tree/development/src/datepicker';
-  componentContentData: ContentSection[] = demoComponentContent;
+  // componentContentData: ContentSection[] = demoComponentContent;
   componentContent: ContentSection[] = demoComponentContent;
-  componentContentOld: ContentSection[] = demoComponentContentOld;
-  content: any;
-
-  _injectors = new Map<ContentSection, ReflectiveInjector>();
 
   constructor(private injector: Injector) { }
-
-  sectionInjections(content: ContentSection) {
-    if (this._injectors.has(content)) {
-      return this._injectors.get(content);
-    }
-
-    const _injector = ReflectiveInjector.resolveAndCreate([
-      {
-        provide: ContentSection,
-        useValue: content
-      }], this.injector);
-
-    this._injectors.set(content, _injector);
-
-    return _injector;
-  }
 
   changeContent(name: string) {
     switch (name) {
       case('old'):
-        this.componentContentData = demoComponentContentOld;
+        this.componentContent = demoComponentContentOld;
         break;
       case('new'):
       default:
-        this.componentContentData = demoComponentContent;
+        this.componentContent = demoComponentContent;
+    }
+
+    if (typeof PR !== 'undefined') {
+      setTimeout(() => PR.prettyPrint(), 10);
     }
   }
 }
