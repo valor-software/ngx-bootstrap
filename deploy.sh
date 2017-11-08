28e559f5d74937294ad3372035f27ddf4a858a6d
sergei.melnik <spawn@sergeimelnil>
2017-11-08 18:37:03 +0200

    fix-deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME_STG
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME_STG
 
-gcloud docker push gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}
+gcloud docker -- push gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}
 
 yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME_STG}/${DOCKER_IMAGE_NAME}:latest
 
