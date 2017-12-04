610cd044154a474603b1f97f8247d537767ec362
Oleksii Aleksandrov <oleksii.aleksandrov@valor-software.com>
2017-12-04 19:14:40 +0200

    fix(accordion demo): fix bootstrap link in API Reference

--- demo/src/app/components/+accordion/accordion-section.component.ts
+++ demo/src/app/components/+accordion/accordion-section.component.ts
@@ -1,5 +1,5 @@
 // todo: add more samples https://jqueryui.com/accordion/#default
-// todo: add more samples http://getbootstrap.com/components/#panels-alternatives
+// todo: add more samples https://getbootstrap.com/docs/3.3/components/#panels-alternatives
 
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { demoComponentContent } from './accordion-section.list';
