d933f12b31828839b975ed85add25b5703ded160
Ilya Surmay <ilya.surmay@valor-software.com>
2017-12-06 13:37:25 +0200

    refactor(build): use await instead of promises

--- scripts/es2015/bundle.es2015.js
+++ scripts/es2015/bundle.es2015.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 const path = require('path');
 const execa = require('execa');
 const fs = require('fs-extra');
@@ -9,28 +9,21 @@ const tmp = '.tmp';
 const dist = 'dist-es2015';
 const tsconfigPath = '.tmp/tsconfig.json';
 
-fs.copy(src, tmp)
-  .then(() => {
-    console.log('Copying src to temp folder');
-    const tsconfig = require(path.resolve(tsconfigPath));
-    tsconfig.compilerOptions.target = 'es2015';
-    tsconfig.compilerOptions.outDir = '../' + dist;
-    return fs.writeFile(tsconfigPath, JSON.stringify(tsconfig), 'utf8')
-  })
-  .then(() => {
-    console.log('Inlining templates and styles');
-    return inlineResources.inlineResources(tmp);
-  })
-  .then(() => {
-    console.log('Compiling library from temp folder');
-    return execa('ngc', ['-p', tmp], { preferLocal: true });
-  })
-  .then(() => {
-    console.log('Bundling es2015 bundle');
-    return execa('rollup --config ./scripts/es2015/es2015.config.js', { shell: true });
-  })
-  .then(() => {
-    console.log('Removing temp folders');
-    del([tmp, dist]);
-  });
+async function createEs2015Bundle() {
+  console.log('Copying src to temp folder');
+  await fs.copy(src, tmp);
+  const tsconfig = require(path.resolve(tsconfigPath));
+  tsconfig.compilerOptions.target = 'es2015';
+  tsconfig.compilerOptions.outDir = '../' + dist;
+  await fs.writeFile(tsconfigPath, JSON.stringify(tsconfig), 'utf8');
+  console.log('Inlining templates and styles');
+  await inlineResources.inlineResources(tmp);
+  console.log('Compiling library from temp folder');
+  await execa('ngc', ['-p', tmp], { preferLocal: true });
+  console.log('Bundling es2015 bundle');
+  await execa('rollup --config ./scripts/es2015/es2015.config.js', { shell: true });
+  console.log('Removing temp folders');
+  await del([tmp, dist]);
+}
+createEs2015Bundle();
 
