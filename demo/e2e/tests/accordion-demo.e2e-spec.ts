5136fd66653b45ea27d3f0896962c9c77034f571
YevheniiaMazur <yevheniia.mazur@valor-software.com>
2017-12-26 16:23:04 +0200

    fix(tests): fixing e2e type errors (#3358)

--- demo/e2e/tests/accordion-demo.e2e-spec.ts
+++ demo/e2e/tests/accordion-demo.e2e-spec.ts
@@ -1,8 +1,9 @@
-import { $, $$, browser, ExpectedConditions } from 'protractor';
+import { $, $$, browser, ElementFinder, ExpectedConditions as EC } from 'protractor';
 import { leftPanelTests } from './leftPanelTests.po';
 import { DataProvider } from '../data-provider/data-provider.po';
 
-let using = require('jasmine-data-provider');
+const using = require('jasmine-data-provider');
+
 const buttonToggleLastPanel = $('accordion-demo>p button:nth-child(1)');
 const buttonEnableDisablePanel = $('accordion-demo>p button:nth-child(2)');
 const buttonAddItem = $('.panel-body .btn');
@@ -10,14 +11,25 @@ const checkboxOnlyOne = $('.checkbox .ng-valid');
 const getItemsCount = $$('accordion-group:nth-child(4) .panel-body > div');
 const buttonArrow = $('.pull-right');
 
-const EC = ExpectedConditions;
+function getTabHeader(tabNumber: number): ElementFinder {
+  return $(`accordion-group:nth-child(${tabNumber}) .panel-heading`);
+}
 
-const getTabHeader = (tabNumber:number) => {
-  return $('accordion-group:nth-child(' + tabNumber + ') .panel-heading');
-};
-const getTabContent = (tabNumber:number) => {
-  return $('accordion-group:nth-child(' + tabNumber + ') .panel-body');
-};
+function getTabContent(tabNumber: number): ElementFinder {
+  return $(`accordion-group:nth-child(${tabNumber}) .panel-body`);
+}
+
+async function tabContentIsDisplayed(tabNum: number): Promise<boolean> {
+  return await getTabContent(tabNum).isDisplayed();
+}
+
+async function tabHeaderIsDisplayed(tabNum: number): Promise<boolean> {
+  return await getTabHeader(tabNum).isDisplayed();
+}
+
+async function getNumberOfItems(): Promise<number> {
+  return await getItemsCount.count();
+}
 
 describe('Check the Accordion page in bootstrap 3', () => {
   beforeAll(() => {
@@ -25,52 +37,69 @@ describe('Check the Accordion page in bootstrap 3', () => {
     leftPanelTests.checkLeftPanelMini();
     leftPanelTests.checkLeftPanelMaxi();
   });
-  it('Close/open first tab by click', () => {
+
+  it('Close/open first tab by click', async () => {
     getTabHeader(1).click();
-    expect(getTabContent(1).isDisplayed()).toBe(false);
+    expect(await tabContentIsDisplayed(1)).toBeFalsy();
+
     getTabHeader(1).click();
-    expect(getTabContent(1).isDisplayed()).toBe(true);
+    expect(await tabContentIsDisplayed(1)).toBeTruthy();
   });
-  it('Open/close last tab with button Toggle Last Panel', () => {
+
+  it('Open/close last tab with button Toggle Last Panel', async () => {
     buttonToggleLastPanel.click();
-    expect(getTabContent(5).isDisplayed()).toBe(true);
+    expect(await tabContentIsDisplayed(5)).toBeTruthy();
     expect(buttonArrow.getAttribute('class')).toContain('glyphicon-chevron-down');
+
     buttonToggleLastPanel.click();
-    expect(getTabContent(5).isDisplayed()).toBe(false);
+    expect(await tabContentIsDisplayed(5)).toBeFalsy();
     expect(buttonArrow.getAttribute('class')).toContain('glyphicon-chevron-right');
   });
-  it('Button Enable/Disable first panel is ON', () => {
+
+  it('Button Enable/Disable first panel is ON', async () => {
     buttonEnableDisablePanel.click();
     getTabHeader(1).click();
-    expect(getTabContent(1).isDisplayed()).toBe(false);
+
+    expect(await tabContentIsDisplayed(1)).toBeFalsy();
   });
-  it('Button Enable/Disable first panel is OFF', () => {
+
+  it('Button Enable/Disable first panel is OFF', async () => {
     buttonEnableDisablePanel.click();
     getTabHeader(1).click();
-    expect(getTabHeader(1).isDisplayed()).toBe(true);
+
+    expect(await tabHeaderIsDisplayed(1)).toBeTruthy();
   });
+
   it('Add items in 4th tab', () => {
     getTabHeader(4).click();
-    expect(getItemsCount.count()).toBe(3);
+    expect(getNumberOfItems).toBe(3);
+
     buttonAddItem.click();
     buttonAddItem.click();
-    expect(getItemsCount.count()).toBe(5);
+    expect(getNumberOfItems).toBe(5);
+
     getTabHeader(4).click();
   });
+
   it('Open all tabs together', () => {
     checkboxOnlyOne.click();
     getTabHeader(1).click();
     browser.wait(EC.visibilityOf(getTabContent(1)), 500);
+
     getTabHeader(2).click();
     browser.wait(EC.visibilityOf(getTabContent(2)), 500);
+
     getTabHeader(3).click();
     browser.wait(EC.visibilityOf(getTabContent(3)), 500);
+
     getTabHeader(4).click();
     browser.wait(EC.visibilityOf(getTabContent(4)), 500);
+
     getTabHeader(5).click();
   });
-  using (DataProvider.accordionTableContent, (data:any, description:string) => {
-    it ('Check table texts: ' + description, () => {
+
+  using(DataProvider.accordionTableContent, (data: any, description: string) => {
+    it(`Check table texts: ${description}`, () => {
       expect(data.element().getText()).toBe(data.actualResult);
     });
   });
--- demo/e2e/tests/tooltip-demo.e2e-spec.ts
+++ demo/e2e/tests/tooltip-demo.e2e-spec.ts
@@ -1,122 +1,137 @@
-import {browser} from 'protractor';
-import {tooltipEl} from '../selectors.json';
+import { browser } from 'protractor';
+import { tooltipEl } from '../selectors.json';
+
+async function tooltipIsDisplayed(): Promise<boolean> {
+  return await tooltipEl.tooltipElement.isDisplayed();
+}
+
+async function getTooltipAttribute(): Promise<string> {
+  return await tooltipEl.buttonComponentLevelStyling.getAttribute('tooltip');
+}
 
 describe('Tooltip page test on bootstrap 3.', () => {
   beforeAll(() => {
     browser.get('#/tooltip');
   });
 
-
   it('Check the tooltip for Simple demo button.', () => {
     browser.actions()
       .mouseMove(tooltipEl.buttonSimpleDemo)
       .perform();
-    expect(tooltipEl.tooltipElement.isDisplayed()).toBe(true);
+
+    expect(tooltipIsDisplayed).toBeTruthy();
   });
 
   it('Four directions tooltip.', () => {
     browser.actions()
       .mouseMove(tooltipEl.buttonFourDirectionsLeft)
       .perform();
-    expect(tooltipEl.tooltipElement.isPresent()).toBe(true);
+    expect(tooltipIsDisplayed).toBeTruthy();
 
     browser.actions()
       .mouseMove(tooltipEl.buttonFourDirectionsTop)
       .perform();
-    expect(tooltipEl.tooltipElement.isPresent()).toBe(true);
+    expect(tooltipIsDisplayed).toBeTruthy();
 
     browser.actions()
       .mouseMove(tooltipEl.buttonFourDirectionsBottom)
       .perform();
-    expect(tooltipEl.tooltipElement.isPresent()).toBe(true);
+    expect(tooltipIsDisplayed).toBeTruthy();
 
     browser.actions()
       .mouseMove(tooltipEl.buttonFourDirectionsRight)
       .perform();
-    expect(tooltipEl.tooltipElement.isPresent()).toBe(true);
+    expect(tooltipIsDisplayed).toBeTruthy();
   });
 
   it('Dismissible tooltip.', () => {
     browser.actions()
       .click(tooltipEl.buttonDismissible)
       .perform();
-    expect(tooltipEl.tooltipElement.isPresent()).toBe(true);
+
+    expect(tooltipIsDisplayed).toBeTruthy();
   });
 
   it('Dynamic Content tooltip.', () => {
     browser.actions()
       .mouseMove(tooltipEl.buttonSimpleBinding)
       .perform();
-    expect(tooltipEl.tooltipElement.isPresent()).toBe(true);
+    expect(tooltipIsDisplayed).toBeTruthy();
 
     browser.actions()
       .mouseMove(tooltipEl.buttonTemplateRefBinding)
       .perform();
-    expect(tooltipEl.tooltipElement.isPresent()).toBe(true);
+    expect(tooltipIsDisplayed).toBeTruthy();
   });
 
   it('Dynamic Html tooltip.', () => {
     browser.actions()
       .mouseMove(tooltipEl.buttonDynamicHTML)
       .perform();
-    expect(tooltipEl.tooltipElement.isPresent()).toBe(true);
+
+    expect(tooltipIsDisplayed).toBeTruthy();
   });
 
   it('Append to body.', () => {
     browser.actions()
       .mouseMove(tooltipEl.buttonDefaultTooltip)
       .perform();
-    expect(tooltipEl.tooltipElement.isPresent()).toBe(true);
+    expect(tooltipIsDisplayed).toBeTruthy();
 
     browser.actions()
       .mouseMove(tooltipEl.buttonAppendedToBody)
       .perform();
-    expect(tooltipEl.tooltipElement.isPresent()).toBe(true);
+    expect(tooltipIsDisplayed).toBeTruthy();
   });
 
   it('Preconfigured tooltip.', () => {
+    const textTemplate = 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus.';
+
     browser.actions()
       .mouseMove(tooltipEl.buttonPreconfiguredTooltip)
       .perform();
-    expect(tooltipEl.buttonPreconfiguredTooltip.getAttribute('tooltip')).toContain("Vivamus sagittis lacus vel augue laoreet rutrum faucibus.");
+
+    expect(getTooltipAttribute).toContain(textTemplate);
   });
 
   it('Custom triggers.', () => {
     browser.actions()
       .mouseMove(tooltipEl.buttonCustomTriggers)
-      .perform();//Moving over button to show tooltip.
-    expect(tooltipEl.tooltipElement.isPresent()).toBe(true);
+      .perform(); // Moving over button to show tooltip.
+    expect(tooltipIsDisplayed).toBeTruthy();
 
     browser.actions()
       .mouseMove(tooltipEl.buttonPreconfiguredTooltip)
-      .perform();//Moving to to another button to test than tooltip still showed.
-    expect(tooltipEl.tooltipElement.isPresent()).toBe(true);
+      .perform(); // Moving to to another button to test than tooltip still showed.
+    expect(tooltipIsDisplayed).toBeTruthy();
 
     browser.actions()
       .click(tooltipEl.buttonCustomTriggers)
-      .perform();//Clicking the button - tooltip must be hidden.
-    expect(tooltipEl.tooltipElement.isPresent()).toBe(false);
+      .perform(); // Clicking the button - tooltip must be hidden.
+    expect(tooltipIsDisplayed).toBeFalsy();
   });
 
   it('Manual triggering.', () => {
-    expect(tooltipEl.tooltipElement.isPresent()).toBe(false);
+    expect(tooltipIsDisplayed).toBeFalsy();
 
     browser.actions()
       .click(tooltipEl.buttonManualTriggeringShow)
       .perform();
-    expect(tooltipEl.tooltipElement.isPresent()).toBe(true);
+    expect(tooltipIsDisplayed).toBeTruthy();
 
     browser.actions()
       .click(tooltipEl.buttonManualTriggeringHide)
       .perform();
-    expect(tooltipEl.tooltipElement.isPresent()).toBe(false);
-
+    expect(tooltipIsDisplayed).toBeFalsy();
   });
 
   it('Component level styling.', () => {
+    const textTemplate = 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus.';
+
     browser.actions()
       .mouseMove(tooltipEl.buttonComponentLevelStyling)
       .perform();
-    expect(tooltipEl.buttonComponentLevelStyling.getAttribute('tooltip')).toContain("Vivamus sagittis lacus vel augue laoreet rutrum faucibus.");
+
+    expect(getTooltipAttribute).toContain(textTemplate);
   });
 });
