1e9e46892373c2d07d53ce944d8d0145708de314
cander2018 <35771423+cander2018@users.noreply.github.com>
2018-01-24 18:22:20 +0200

    Update demo-datepicker.module.ts

--- demo/src/app/components/+datepicker/demo-datepicker.module.ts
+++ demo/src/app/components/+datepicker/demo-datepicker.module.ts
@@ -8,7 +8,7 @@ import { BsDatepickerModule, DatepickerModule } from 'ngx-bootstrap/datepicker';
 import {
   arLocale, csLocale, daLocale, deLocale, enGbLocale, esDoLocale, esLocale, esUsLocale, frLocale, heLocale, hiLocale,
   huLocale, itLocale, jaLocale, koLocale, nlBeLocale, nlLocale, plLocale, ptBrLocale, ruLocale, svLocale, thLocale,
-  trLocale, zhCnLocale, ukCnLocale
+  trLocale, zhCnLocale, ukLocale
 } from 'ngx-bootstrap/locale';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 
@@ -20,7 +20,7 @@ import { DEMO_COMPONENTS } from './demos';
 const locales = [
   arLocale, csLocale, daLocale, deLocale, enGbLocale, esLocale, esDoLocale, esUsLocale,
   frLocale, hiLocale, heLocale, huLocale, itLocale, jaLocale, koLocale, nlLocale,
-  nlBeLocale, plLocale, ptBrLocale, svLocale, ruLocale, zhCnLocale, trLocale, thLocale, ukCnLocale
+  nlBeLocale, plLocale, ptBrLocale, svLocale, ruLocale, zhCnLocale, trLocale, thLocale, ukLocale
 ];
 locales.forEach((locale: LocaleData) => defineLocale(locale.abbr, locale));
 
