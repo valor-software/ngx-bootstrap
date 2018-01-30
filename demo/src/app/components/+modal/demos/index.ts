13e652c3275003738b43c8e77f87f95f9e0be266
olshansky <olshansky0@gmail.com>
2018-01-30 15:12:23 +0200

    docs(modal): decompose Options demo
    Decompose option demo to: custom css class, animation, esc button and backdrop options
    
    close #3583

--- demo/src/app/components/+modal/demos/index.ts
+++ demo/src/app/components/+modal/demos/index.ts
@@ -8,7 +8,10 @@ import { DemoModalEventsComponent } from './events/events';
 import { DemoModalServiceStaticComponent } from './service-template/service-template';
 import { DemoModalServiceFromComponent } from './service-component/service-component';
 import { DemoModalServiceNestedComponent } from './service-nested/service-nested';
-import { DemoModalServiceOptionsComponent } from './service-options/service-options';
+import { DemoModalServiceDisableAnimationComponent } from './service-options/disable-animation/disable-animation';
+import { DemoModalServiceCustomCSSClassComponent } from './service-options/custom-css-class/custom-css-class';
+import { DemoModalServiceDisableEscClosingComponent } from './service-options/disable-esc-closing/disable-esc-closing';
+import { DemoModalServiceDisableBackdropComponent } from './service-options/disable-backdrop/disable-backdrop';
 import { DemoModalServiceEventsComponent } from './service-events/service-events';
 import { DemoModalServiceConfirmWindowComponent } from './service-confirm-window/service-confirm-window';
 
@@ -21,7 +24,10 @@ export const DEMO_COMPONENTS = [
   DemoModalServiceStaticComponent,
   DemoModalServiceFromComponent,
   DemoModalServiceNestedComponent,
-  DemoModalServiceOptionsComponent,
+  DemoModalServiceCustomCSSClassComponent,
+  DemoModalServiceDisableAnimationComponent,
+  DemoModalServiceDisableEscClosingComponent,
+  DemoModalServiceDisableBackdropComponent,
   DemoModalEventsComponent,
   DemoModalServiceEventsComponent,
   DemoModalServiceConfirmWindowComponent
