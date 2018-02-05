83ba3a62913bc9dc188a9e78ba3a1d2edda5b120
olshansky <olshansky0@gmail.com>
2018-02-05 14:46:24 +0200

    docs(carousel): decomopose Dynamic Slides
    Decompose Dynamic Slides section to: Dynamic slides, Disable indicator, Disable slide looping, Interval
    
    close #3651

--- demo/src/app/components/+carousel/demos/index.ts
+++ demo/src/app/components/+carousel/demos/index.ts
@@ -2,10 +2,16 @@ import { DemoCarouselDynamicComponent } from './dynamic/dynamic';
 import { DemoCarouseBasicComponent } from './basic/basic';
 import { DemoCarouselCaptionsComponent } from './captions/captions';
 import { DemoCarouselConfigComponent } from './config/config';
+import { DemoCarouselIntervalComponent } from './interval/interval';
+import { DemoCarouselDisableIndicatorComponent } from './disable-indicator/disable-indicator';
+import { DemoCarouselDisableLoopingComponent } from './disable-looping/disable-looping';
 
 export const DEMO_COMPONENTS = [
   DemoCarouselDynamicComponent,
   DemoCarouseBasicComponent,
   DemoCarouselCaptionsComponent,
-  DemoCarouselConfigComponent
+  DemoCarouselConfigComponent,
+  DemoCarouselIntervalComponent,
+  DemoCarouselDisableIndicatorComponent,
+  DemoCarouselDisableLoopingComponent
 ];
