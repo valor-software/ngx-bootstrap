bd085bf9166c6746625842825e0b365a65f0a367
tarusin <tarusin.iliya@gmail.com>
2018-02-06 19:31:15 +0300

    docs(collapse): add demo with show/hide methods (#3599)
    * docs(collapse): add demo with show/hide methods
    close #3591
    
    * docs(collapse): using CollapseDirective

---
+++ demo/src/app/components/+collapse/demos/toggle-manual/toggle-manual.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'toggle-manual-demo',
+  templateUrl: './toggle-manual.html'
+})
+export class ToggleManualDemoComponent {
+  isOpen = false;
+}
