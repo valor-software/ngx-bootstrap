1eb83808bb0ac4a3939e2cdb95c48a2c03e74205
olshansky <olshansky0@gmail.com>
2018-02-06 14:36:55 +0200

    docs(pagination): rename Default section to Basic and decompose
    close #3680

--- demo/src/app/components/+pagination/demos/index.ts
+++ demo/src/app/components/+pagination/demos/index.ts
@@ -2,10 +2,22 @@ import { DemoPaginationBasicComponent } from './basic/basic';
 import { DemoPaginationPagerComponent } from './pager/pager';
 import { DemoPaginationLimitComponent } from './limit/limit';
 import { DemoPaginationStylingComponent } from './styling-global/styling-global';
+import { DemoPaginationManualSwitchingComponent } from './manual-switching/manual-switching';
+import { DemoPaginationPageChangedComponent } from './page-changed-event/page-changed-event';
+import { DemoPaginationPagesCountChangedComponent } from './pages-count-changed/pages-count-changed';
+import { DemoPaginationBoundaryLinksComponent } from './boundary-links/boundary-links';
+import { DemoPaginationDirectionLinksComponent } from './direction-links/direction-links';
+import { DemoPaginationCustomLinksContentComponent } from './custom-links-content/custom-links-content';
 
 export const DEMO_COMPONENTS = [
   DemoPaginationBasicComponent,
   DemoPaginationPagerComponent,
   DemoPaginationLimitComponent,
-  DemoPaginationStylingComponent
+  DemoPaginationStylingComponent,
+  DemoPaginationManualSwitchingComponent,
+  DemoPaginationPageChangedComponent,
+  DemoPaginationPagesCountChangedComponent,
+  DemoPaginationBoundaryLinksComponent,
+  DemoPaginationDirectionLinksComponent,
+  DemoPaginationCustomLinksContentComponent
 ];
