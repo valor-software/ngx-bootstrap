80012dbdd6a41ec2cc2489350d84454ad5965a43
tarusin <iliya.tarusin@valor-software.com>
2018-02-07 16:38:58 +0200

    fix(datepicker): testing start errors

--- demo/src/app/components/+datepicker/demos/hide-on-scroll/hide-on-scroll.ts
+++ demo/src/app/components/+datepicker/demos/hide-on-scroll/hide-on-scroll.ts
@@ -1,5 +1,4 @@
 import { Component, HostListener, ViewChild } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
 import { BsDatepickerDirective } from 'ngx-bootstrap/datepicker';
 
 @Component({
@@ -8,10 +7,6 @@ import { BsDatepickerDirective } from 'ngx-bootstrap/datepicker';
 })
 export class DemoDatepickerHideOnScrollComponent {
   @ViewChild(BsDatepickerDirective) datepicker: BsDatepickerDirective;
-
-  myForm = new FormGroup({
-    myDate: new FormControl(new Date())
-  });
 
   @HostListener('window:scroll')
   onScrollEvent() {
