c3c13d7ddefb486eed913030979442b08d38cded
svetoldo4444ka <svetlana.korotkykh@valor-software.com>
2018-02-07 16:56:39 +0200

    docs(tabs): decompose Static tabs demo

--- demo/src/app/components/+datepicker/datepicker-section.list.ts
+++ demo/src/app/components/+datepicker/datepicker-section.list.ts
@@ -203,3 +203,4 @@ export const demoComponentContentOld: ContentSection[] = [
     ]
   }
 ];
+
--- demo/src/app/components/+datepicker/demos/index.ts
+++ demo/src/app/components/+datepicker/demos/index.ts
@@ -21,3 +21,5 @@ export const DEMO_COMPONENTS = [
   DemoDatepickerReactiveFormsComponent,
   DemoDatepickerPlacementComponent
 ];
+
+
