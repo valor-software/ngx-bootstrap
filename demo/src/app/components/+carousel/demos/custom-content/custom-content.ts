47bb1987329090a8431ec52e40ca5c928f6d8504
tarusin <iliya.tarusin@valor-software.com>
2018-02-13 15:20:06 +0200

    docs(carousel): remove custom styles

--- demo/src/app/components/+carousel/demos/custom-content/custom-content.ts
+++ demo/src/app/components/+carousel/demos/custom-content/custom-content.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'demo-carousel-custom-content',
-  styles: ['.jumbotron { margin: 0; background-color: #6c757d; color: #fff; }'],
   templateUrl: './custom-content.html'
 })
 export class DemoCarouselCustomContentComponent {}
