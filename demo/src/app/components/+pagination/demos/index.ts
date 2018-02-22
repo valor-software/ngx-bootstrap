85b1aeb9242b8ee126c38af08658dfd91b1a46a5
Artur <olshansky0@gmail.com>
2018-02-22 14:58:36 +0200

    docs(pagination): Add demo for content switching (#3785)
    close #3771

--- demo/src/app/components/+pagination/demos/index.ts
+++ demo/src/app/components/+pagination/demos/index.ts
@@ -2,10 +2,12 @@ import { DemoPaginationBasicComponent } from './basic/basic';
 import { DemoPaginationPagerComponent } from './pager/pager';
 import { DemoPaginationLimitComponent } from './limit/limit';
 import { DemoPaginationStylingComponent } from './styling-global/styling-global';
+import { DemoPaginationContentSwitchingComponent } from './content-switching/content-switching';
 
 export const DEMO_COMPONENTS = [
   DemoPaginationBasicComponent,
   DemoPaginationPagerComponent,
   DemoPaginationLimitComponent,
-  DemoPaginationStylingComponent
+  DemoPaginationStylingComponent,
+  DemoPaginationContentSwitchingComponent
 ];
