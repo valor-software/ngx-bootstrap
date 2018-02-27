0bf689ea28f06572fa060fc448ee3ce0660a23d4
Ilya Surmay <ilya.surmay@valor-software.com>
2018-02-27 13:08:50 +0200

    fix(typeahead): fix switchMap import

--- src/typeahead/typeahead.directive.ts
+++ src/typeahead/typeahead.directive.ts
@@ -18,6 +18,7 @@ import 'rxjs/add/observable/from';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/toArray';
 
