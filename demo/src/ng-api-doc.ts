e4ca6b2b9b4a25f0bb6d677b40c830ef675b5179
Arpad Vas <avas@mifinity.com>
2018-02-28 10:05:38 +0100

    refactor(docs): change select and deselect outputs

--- demo/src/ng-api-doc.ts
+++ demo/src/ng-api-doc.ts
@@ -2667,7 +2667,7 @@ export const ngdoc: any = {
     ],
     "outputs": [
       {
-        "name": "deselect",
+        "name": "deselectTab",
         "description": "<p>fired when tab became inactive, $event:Tab equals to deselected instance of Tab component </p>\n"
       },
       {
@@ -2675,7 +2675,7 @@ export const ngdoc: any = {
         "description": "<p>fired before tab will be removed, $event:Tab equals to instance of removed tab </p>\n"
       },
       {
-        "name": "select",
+        "name": "selectTab",
         "description": "<p>fired when tab became active, $event:Tab equals to selected instance of Tab component </p>\n"
       }
     ],
