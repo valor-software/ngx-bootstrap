cb62951cba37e5725a3fa6dc547e76771ee9ee5a
Daniel <mytechde@outlook.com>
2018-03-13 19:01:39 +0100

    ref(demo): use HttpClient instead of the old Http module

--- demo/src/app/theme/style-manager.ts
+++ demo/src/app/theme/style-manager.ts
@@ -29,9 +29,7 @@ function getLinkElementForKey(key: string) {
 }
 
 function getExistingLinkElementByKey(key: string) {
-  return document.head.querySelector(
-    `link[rel="stylesheet"].${getClassNameForKey(key)}`
-  );
+  return document.head.querySelector(`link[rel="stylesheet"].${getClassNameForKey(key)}`);
 }
 
 function createLinkElementWithKey(key: string) {
