5373b5d11259b8b32a22aa96330200fcad250b97
Yevheniia Mazur <yevheniia.mazur@valor-software.com>
2018-03-26 17:18:15 +0300

    docs(popover): rename Trigger by input demo

---
+++ demo/src/app/components/+popover/demos/trigger-by-isopen-property/trigger-by-isopen-property.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-popover-trigger-by-isopen',
+  templateUrl: './trigger-by-isopen-property.html'
+})
+export class DemoPopoverByIsOpenPropComponent {
+  isOpen = false;
+}
