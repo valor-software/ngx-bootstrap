9f2ec9250249f33d124fab79a2b18434d1909ecd
Ludmila Nesvitiy <ludmila.nesvitiy@valor-software.com>
2018-11-07 13:18:39 +0200

    fix(tests): cleanup test code (#4778)

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,5 @@
-const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
+const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
 
 module.exports = on => {
   on('file:preprocessor', cypressTypeScriptPreprocessor)
-}
+};
