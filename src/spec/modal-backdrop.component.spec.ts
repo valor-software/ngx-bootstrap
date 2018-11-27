8a3ad95ffcd9e72ef159452457e2d0dd40065191
svetoldo4444ka <svetlana.korotkykh@valor-software.com>
2018-11-27 12:45:44 +0200

    feat(modal-backdrop): added tests to modal-backdrop component

--- src/spec/modal-backdrop.component.spec.ts
+++ src/spec/modal-backdrop.component.spec.ts
@@ -1,5 +1,8 @@
-import { ModalBackdropComponent } from '../modal';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ModalBackdropComponent } from '../modal';
+import { window } from '../utils/facade/browser';
 
 describe('ModalBackdropComponent tests', () => {
   let fixture: ComponentFixture<ModalBackdropComponent>;
@@ -11,22 +14,53 @@ describe('ModalBackdropComponent tests', () => {
     });
     fixture = TestBed.createComponent(ModalBackdropComponent);
     component = fixture.componentInstance;
+    component.isAnimated = true;
     fixture.detectChanges();
   });
 
   it('should get and set isAnimated correctly', () => {
+    component.isAnimated = false;
+    expect(component.isAnimated).toBeFalsy();
+
     component.isAnimated = true;
     expect(component.isAnimated).toBeTruthy();
   });
 
-  it('isShown() tests', () => {
+  it('should get and set isShown correctly', () => {
+    component.isShown = false;
+    expect(component.isShown).toBeFalsy();
+
+    component.isShown = true;
+    expect(component.isShown).toBeTruthy();
+  });
+
+  it('isShown() tests, when bs4', () => {
+    const tempVal = window.__theme;
+    window.__theme = 'bs4';
+    component.isShown = true;
+    fixture.detectChanges();
+    let modalClass = component.element.nativeElement.classList.contains('show');
+    expect(modalClass).toBeTruthy();
+
+    component.isShown = false;
+    fixture.detectChanges();
+    modalClass = component.element.nativeElement.classList.contains('show');
+    expect(modalClass).toBeFalsy();
+    window.__theme = tempVal;
+  });
+
+  it('isShown() tests, when bs3', () => {
+    const tempVal = window.__theme;
+    window.__theme = 'bs3';
     component.isShown = true;
     fixture.detectChanges();
     let modalClass = component.element.nativeElement.classList.contains('in');
     expect(modalClass).toBeTruthy();
+
     component.isShown = false;
     fixture.detectChanges();
     modalClass = component.element.nativeElement.classList.contains('in');
     expect(modalClass).toBeFalsy();
+    window.__theme = tempVal;
   });
 });
