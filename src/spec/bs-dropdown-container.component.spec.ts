cd6226b4ba6209914583f222647f130ee783480e
svetoldo4444ka <svetlana.korotkykh@valor-software.com>
2018-12-04 15:09:45 +0200

    fix(dropdown): tests

--- src/spec/bs-dropdown-container.component.spec.ts
+++ src/spec/bs-dropdown-container.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BsDropdownContainerComponent, BsDropdownModule } from '../dropdown';
+import { BsDropdownContainerComponent, BsDropdownModule, BsDropdownState } from '../dropdown';
+import { Subject } from 'rxjs';
+import { window } from '../utils';
 
 describe('BsDropdownContainerComponent tests', () => {
   let fixture: ComponentFixture<BsDropdownContainerComponent>;
   let component: BsDropdownContainerComponent;
+  /* tslint:disable-next-line:no-inferred-empty-object-type */
+  const stateSubject = new Subject();
+  let fakeService;
 
   beforeEach(() => {
+    fakeService = {
+      isOpenChange: stateSubject.asObservable()
+    };
     TestBed.configureTestingModule({
-      imports: [BsDropdownModule.forRoot()]
+      imports: [BsDropdownModule.forRoot()],
+      providers: [{ provide: BsDropdownState, useValue: fakeService }]
     });
   });
 
@@ -22,13 +31,15 @@ describe('BsDropdownContainerComponent tests', () => {
     expect(component).not.toBeNull();
   });
 
-  it('should set isOpen property', () => {
-    component.isOpen = true;
-    
-    expect(component.isOpen).toBeTruthy();
-  });
+  it('should be call isOpenChange method', () => {
+    const tempVal = window.__theme;
+    window.__theme = 'bs4';
+    const spy = spyOn((component as any).cd, 'detectChanges');
 
-  it('should contain direction property', () => {
-    expect(component.direction).toEqual('down');
+    stateSubject.next(true);
+    fixture.detectChanges();
+
+    expect(spy).toHaveBeenCalled();
+    window.__theme = tempVal;
   });
 });
