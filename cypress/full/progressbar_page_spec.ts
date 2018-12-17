e94a0d1dbd8b410401206ec52c243bd82dd4ad28
ludmilanesvitiy <ludmila.nesvitiy@careerbuilder.com>
2018-12-17 17:52:49 +0200

    feat(tests): add full e2e coverage for Tabs component

--- cypress/full/progressbar_page_spec.ts
+++ cypress/full/progressbar_page_spec.ts
@@ -30,25 +30,6 @@ describe('Progressbar demo page test suite', () => {
 
   beforeEach(() => progressbar.navigateTo());
 
-  describe('Static', () => {
-    const staticBars = progressbar.exampleDemosArr.static;
-
-    it('User see 3 progressbars, 1 value matches config, 2 - "warning", 3 - "danger", animation', () => {
-      progressbar.scrollToMenu('Static');
-      progressbar.isProgressbarHaveAttrs(staticBars, [{ attr: 'max', value: '100' }], 0);
-      progressbar.isProgressbarHaveAttrs(staticBars, [{ attr: 'type', value: 'warning' },
-        { attr: 'max', value: '100' }], 1);
-      progressbar.isProgressbarHaveAttrs(staticBars, [{ attr: 'type', value: 'danger' },
-        { attr: 'max', value: '200' }], 2);
-
-      progressbar.isBarHaveAttrs(staticBars, [{ attr: 'aria-valuetext', value: '55%' }], 0);
-      progressbar.isBarHaveAttrs(staticBars, [{ attr: 'aria-valuetext', value: '22%' }], 1);
-      progressbar.isBarHaveAttrs(staticBars, [{ attr: 'aria-valuetext', value: '83%' },
-        { attr: 'aria-valuenow', value: '166' }], 2);
-      progressbar.isBarHaveAnimationCSS(staticBars, true, 2);
-    });
-  });
-
   describe('Dynamic', () => {
     const dynamic = progressbar.exampleDemosArr.dynamic;
 
