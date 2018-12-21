58dd1dcffbe019686505ed1f1b250e4ffbfa3ad2
svetoldo4444ka <svetlana.korotkykh@valor-software.com>
2018-12-21 19:35:51 +0200

    fix(landing): fix cypress tests after chahge langing

--- cypress/support/landing.po.ts
+++ cypress/support/landing.po.ts
@@ -15,7 +15,6 @@ export class LandingPo extends BaseComponent {
   stackoverflowUrl = 'https://stackoverflow.com/questions/tagged/ngx-bootstrap';
   githubUrl = 'https://github.com/valor-software/ngx-bootstrap';
   slackUrl = 'https://join.slack.com/t/ngx-home/shared_invite/enQtNTExMTY5MzcwMTM0LWQ5M2Y4OWM0OGJjNmZiOGYyNjFlZTdlOGI1YjcxYWQ2ODhiOTY4NzhiODgwMTIzNDczODIyNWNmM2RlYWRhNTg';
-
   teamUrl = 'https://github.com/valor-software';
   contributorsUrl = 'https://github.com/valor-software/ngx-bootstrap/graphs/contributors';
   mitLicenseUrl = 'https://github.com/valor-software/ngx-bootstrap/blob/development/LICENSE';
