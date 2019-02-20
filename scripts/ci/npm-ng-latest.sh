ae089c72d05edecff59cd1e3abccb464c482bab0
Vitaliy Makogon <mvitaliyd@gmail.com>
2019-02-20 17:39:18 +0200

    refactor(common): migrate to angular 7

--- scripts/ci/npm-ng-latest.sh
+++ scripts/ci/npm-ng-latest.sh
@@ -2,20 +2,22 @@
 
 npm uninstall @angular/tsc-wrapped
 npm i @angular/animations@latest \
+    @angular-devkit/core@latest \
+    @angular-devkit/schematics@latest \
+    @angular/cli@latest \
     @angular/common@latest \
-    @angular/compiler@latest \
     @angular/compiler-cli@latest \
-    @angular/core@latest \
+    @angular/compiler@latest \
+    @angular/core@8.0.0-beta.4 \
     @angular/forms@latest \
     @angular/http@latest \
     @angular/language-service@latest \
+    @angular/platform-browser-dynamic@latest \
     @angular/platform-browser@latest \
     @angular/platform-server@latest \
-    @angular/platform-browser-dynamic@latest \
     @angular/router@latest \
     @angular/service-worker@latest \
-    @angular-devkit/schematics@latest \
     @schematics/angular@latest \
     typescript@3.2.2 \
-    tsickle@0.33.0 \
+    tsickle@0.34.0 \
     rxjs@6.3
