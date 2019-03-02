c33210e8d0325ff21914c80972a03a028471980c
Vitaliy Makogon <mvitaliyd@gmail.com>
2019-03-03 00:15:56 +0200

    fix(timepicker): fix imports

--- scripts/ci/update-pkg.js
+++ scripts/ci/update-pkg.js
@@ -2,22 +2,18 @@
 
 const fs = require('fs-extra');
 const pathToJSONFile = 'dist/package.json';
-const NGX_BOOTSRAP = 'ngx-bootstrap';
-const NGX_BOOTSRAP_CI = 'ngx-bs-ci';
+const NGX_BOOTSRAP_CI = 'ngx-bootstrap-ci';
 
-changeName();
+updatePkg();
 
-async function changeName() {
+async function updatePkg() {
   const packageJson = JSON.parse(fs.readFileSync(pathToJSONFile), 'utf8');
   const time = (new Date).getTime();
 
   packageJson.version = `0.0.${time}`;
-
-  packageJson.name = (packageJson.name === NGX_BOOTSRAP)
-    ? NGX_BOOTSRAP_CI
-    : NGX_BOOTSRAP;
+  packageJson.name = NGX_BOOTSRAP_CI;
 
   fs.writeFile(pathToJSONFile, JSON.stringify(packageJson, null, 2));
 
-  console.log(`Name of package changed to ${packageJson.name}`);
+  console.log(`Package.json was updated`);
 }
