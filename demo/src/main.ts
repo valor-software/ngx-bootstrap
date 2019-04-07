29e86a9768497f3fb6a5b0f5fe071ed991799fe7
Vitaliy Makogon <mvitaliyd@gmail.com>
2019-04-07 21:41:41 +0300

    fix(demo): location is not defined

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -10,18 +10,3 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
-
-
-// import { enableProdMode } from '@angular/core';
-// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-//
-// import { AppModule } from './app/app.module';
-// import { environment } from './environments/environment';
-//
-// if (environment.production) {
-//   enableProdMode();
-// }
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//   platformBrowserDynamic().bootstrapModule(AppModule);
-// });
