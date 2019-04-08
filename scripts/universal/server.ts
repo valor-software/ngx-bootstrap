47b7537f9d6d9bccfdcc15574107f76a3bfb8560
Vitaliy Makogon <mvitaliyd@gmail.com>
2019-04-08 22:02:34 +0300

    feat(build): update nestjs-universal config (#5156)

--- scripts/universal/server.ts
+++ scripts/universal/server.ts
@@ -8,10 +8,6 @@ enableProdMode();
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
-  app.enableCors({
-    methods: 'GET',
-    maxAge: 3600,
-  });
   await app.listen(PORT);
 }
 bootstrap().catch(err => console.error(err));
