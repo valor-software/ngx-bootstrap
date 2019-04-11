e7dec4a0cc47b4a53b1777e7fff1341ec715087f
Vitaliy Makogon <mvitaliyd@gmail.com>
2019-04-11 12:52:14 +0300

    fix(common): fix collapse and accordion components tests

--- src/collapse/collapse-animations.ts
+++ src/collapse/collapse-animations.ts
@@ -4,15 +4,14 @@ import {
   AnimationMetadata
 } from '@angular/animations';
 
-/** Time and timing curve for expansion panel animations. */
-export const EXPANSION_PANEL_ANIMATION_TIMING = '400ms cubic-bezier(0.4,0.0,0.2,1)';
+export const COLLAPSE_ANIMATION_TIMING = '400ms cubic-bezier(0.4,0.0,0.2,1)';
 
 export const expandAnimation: AnimationMetadata[] = [
   style({ height: 0, visibility: 'hidden', overflow: 'hidden' }),
-  animate(EXPANSION_PANEL_ANIMATION_TIMING, style({ height: '*', visibility: 'visible' }))
+  animate(COLLAPSE_ANIMATION_TIMING, style({ height: '*', visibility: 'visible' }))
 ];
 
 export const collapseAnimation: AnimationMetadata[] = [
   style({ height: '*', visibility: 'visible', overflow: 'hidden' }),
-  animate(EXPANSION_PANEL_ANIMATION_TIMING, style({ height: 0, visibility: 'hidden' }))
+  animate(COLLAPSE_ANIMATION_TIMING, style({ height: 0, visibility: 'hidden' }))
 ];
