c102ff0ca3cede7a8e952e1e60ab288a9130749d
Vitaliy Makogon <mvitaliyd@gmail.com>
2019-06-27 20:01:20 +0300

    feat(doc): enable ivy (#5285)
    * feat(doc): enable ivy engine for demo

--- src/datepicker/themes/bs/bs-datepicker-inline-container.component.ts
+++ src/datepicker/themes/bs/bs-datepicker-inline-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { BsDatepickerContainerComponent } from './bs-datepicker-container.component';
 
 import { BsDatepickerConfig } from '../../bs-datepicker.config';
@@ -14,14 +14,15 @@ import { datepickerAnimation } from '../../datepicker-animations';
   providers: [BsDatepickerStore, BsDatepickerEffects],
   templateUrl: './bs-datepicker-view.html',
   host: {
-    '(click)': '_stopPropagation($event)',
-    style: 'display: inline-block;'
+    '(click)': '_stopPropagation($event)'
   },
   animations: [datepickerAnimation]
 })
 export class BsDatepickerInlineContainerComponent extends BsDatepickerContainerComponent
   implements OnInit, OnDestroy {
+
   constructor(
+    _renderer: Renderer2,
     _config: BsDatepickerConfig,
     _store: BsDatepickerStore,
     _element: ElementRef,
@@ -29,6 +30,9 @@ export class BsDatepickerInlineContainerComponent extends BsDatepickerContainerC
     _effects: BsDatepickerEffects,
     _positioningService: PositioningService
   ) {
-    super(_config, _store, _element, _actions, _effects, _positioningService);
+    super(_renderer, _config, _store, _element, _actions, _effects, _positioningService);
+
+    _renderer.setStyle(_element.nativeElement, 'display', 'inline-block');
+    _renderer.setStyle(_element.nativeElement, 'position', 'static');
   }
 }
