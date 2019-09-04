11eec1d7f84e1fbadd16838f6358a3191483241f
Vitaliy Makogon <mvitaliyd@gmail.com>
2019-09-04 15:20:19 +0300

    fix(datepicker): fix timezone issue

--- scripts/ivy/compiler-cli-fix.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Remove when be fixed https://github.com/angular/angular/issues/22524
-
-if grep -q 'if (sf\[`redirectInfo`\]) { sf = sf\[`redirectInfo`\].redirectTarget; }' node_modules/@angular/compiler-cli/src/ngtsc/typecheck/src/context.js; then
-  echo "compiler-cli already fixed"
-else
-  sed -i '/var sf = this.transform(originalSf);/a if (sf[`redirectInfo`]) { sf = sf[`redirectInfo`].redirectTarget; }' node_modules/@angular/compiler-cli/src/ngtsc/typecheck/src/context.js
-  echo "compiler-cli fixed"
-fi
