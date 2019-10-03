6af841664f157ff36390ea09d1e101608b572145
Vitaliy Makogon <mvitaliyd@gmail.com>
2019-10-03 12:25:20 +0300

    fix(travis): fix next stage (#5419)

--- scripts/ci/npm-ng-next.sh
+++ scripts/ci/npm-ng-next.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
-npm i @angular/animations@next \
-    @angular/core@next \
-    @angular-devkit/core@next \
-    @angular-devkit/schematics@next \
-    @angular/cli@next \
-    @angular/common@next \
-    @angular/compiler-cli@next \
-    @angular/compiler@next \
-    @angular/forms@next \
-    @angular/http@next \
-    @angular/language-service@next \
-    @angular/platform-browser-dynamic@next \
-    @angular/platform-browser@next \
-    @angular/platform-server@next \
-    @angular/router@next \
-    @angular/service-worker@next \
-    @schematics/angular@next \
+npm i @angular/animations@9.0.0-next.2 \
+    @angular/core@9.0.0-next.2 \
+    @angular-devkit/core@9.0.0-next.2 \
+    @angular-devkit/schematics@9.0.0-next.2 \
+    @angular/cli@9.0.0-next.2 \
+    @angular/common@9.0.0-next.2 \
+    @angular/compiler-cli@9.0.0-next.2 \
+    @angular/compiler@9.0.0-next.2 \
+    @angular/forms@9.0.0-next.2 \
+    @angular/language-service@9.0.0-next.2 \
+    @angular/platform-browser-dynamic@9.0.0-next.2 \
+    @angular/platform-browser@9.0.0-next.2 \
+    @angular/platform-server@9.0.0-next.2 \
+    @angular/router@9.0.0-next.2 \
+    @angular/service-worker@9.0.0-next.2 \
+    @schematics/angular@9.0.0-next.2 \
     typescript@3.5.3 \
     tsickle@0.35.0 \
     rxjs@6.5.2
