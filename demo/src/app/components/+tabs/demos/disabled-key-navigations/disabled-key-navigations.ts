3dac6c6f82100910e54eb940e00e1f533fbb558f
bencev <bence312@gmail.com>
2019-10-31 10:33:48 +0100

    add demo, add keynavigations: true to demo default config

---
+++ demo/src/app/components/+tabs/demos/disabled-key-navigations/disabled-key-navigations.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-disabled-key-navigations',
+  templateUrl: './disabled-key-navigations.html'
+})
+export class DemoDisabledKeyNavigationsComponent {
+}
