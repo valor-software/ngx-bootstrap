ebed30b1cafe7033d9d8a1a0afa8ffa8a048b770
lehetap <pavlo.leheta@gmail.com>
2020-05-14 00:39:58 -0400

    feat(typeahead): add feature to do multiple search w/ specified delimiter
    feature allows to show typeahead dropdown even after any value
    already entered by typing one of provided delimiters to be able to pick
    again; keeps all searched values, described in #5781
    
    Closes #5781

--- demo/src/ng-api-doc.ts
+++ demo/src/ng-api-doc.ts
@@ -4013,7 +4013,12 @@ export const ngdoc: any = {
     "description": "",
     "selector": "typeahead-container",
     "inputs": [],
-    "outputs": [],
+    "outputs": [
+      {
+        "name": "activeChange",
+        "description": ""
+      }
+    ],
     "properties": [],
     "methods": []
   },
@@ -4144,6 +4149,17 @@ export const ngdoc: any = {
         "description": "<p>minimal no of characters that needs to be entered before\ntypeahead kicks-in. When set to 0, typeahead shows on focus with full\nlist of options (limited as normal by typeaheadOptionsLimit)</p>\n"
       },
       {
+        "name": "typeaheadMultipleSearch",
+        "type": "boolean",
+        "description": "<p>Can be used to conduct a search of multiple items and have suggestion not for the\nwhole value of the input but for the value that comes after a delimiter provided via\ntypeaheadMultipleSearchDelimiters attribute. This option can only be used together with\ntypeaheadSingleWords option if typeaheadWordDelimiters and typeaheadPhraseDelimiters\nare different from typeaheadMultipleSearchDelimiters to avoid conflict in determining\nwhen to delimit multiple searches and when a single word.</p>\n"
+      },
+      {
+        "name": "typeaheadMultipleSearchDelimiters",
+        "defaultValue": ",",
+        "type": "string",
+        "description": "<p>should be used only in case typeaheadMultipleSearch attribute is true.\nSets the multiple search delimiter to know when to start a new search. Defaults to comma.\nIf space needs to be used, then explicitly set typeaheadWordDelimiters to something else than space\nbecause space is used by default OR set typeaheadSingleWords attribute to false if you don&#39;t need\nto use it together with multiple search.</p>\n"
+      },
+      {
         "name": "typeaheadOptionField",
         "type": "string",
         "description": "<p>when options source is an array of objects, the name of field\nthat contains the options value, we use array item as option in case\nof this field is missing. Supports nested properties and methods.</p>\n"
@@ -4220,8 +4236,8 @@ export const ngdoc: any = {
     ],
     "properties": [
       {
-        "name": "_container",
-        "type": "TypeaheadContainerComponent",
+        "name": "activeDescendant",
+        "type": "string",
         "description": "<p>if false restrict model values to the ones selected from the popup only will be provided\nif false the first match automatically will not be focused as you type\nformat the ng-model result after selection\nif true automatically select an item when there is one option that exactly matches the user input\nif true select the currently highlighted match on blur\nif false don&#39;t focus the input element the typeahead directive is associated with on selection</p>\n"
       }
     ],
