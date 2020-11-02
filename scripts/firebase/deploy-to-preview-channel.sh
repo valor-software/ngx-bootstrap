9db19552fcb12a7e4723d35abba1b96eaefd4b36
NikitaGlukhi <nikita.glukhi@valor-software.com>
2020-11-02 18:12:56 +0200

    refactor(ci): refactored requests to github api in bash script

--- scripts/firebase/deploy-to-preview-channel.sh
+++ scripts/firebase/deploy-to-preview-channel.sh
@@ -10,7 +10,7 @@ EXPIRE_TIME_UTC=`echo ${NGX_BOOTSTRAP_DEMO} | jq -r .expireTime`
 EXPIRE_TIME=$(TZ='GMT' date -d $EXPIRE_TIME_UTC +%c)
 
 NEW_COMMENT="Project: $SITE \n Url: $URL \n This link will expire at $EXPIRE_TIME"
-COMMENTS=$(curl -H "Authorization: token $GITHUB_TOKEN" -X GET "https://api.github.com/repos/$TRAVIS_REPO_SLUG/issues/$TRAVIS_PULL_REQUEST/comments")
+COMMENTS=$(curl -X GET "https://api.github.com/repos/$TRAVIS_REPO_SLUG/issues/$TRAVIS_PULL_REQUEST/comments")
 
 SUBSTRING="Project: ngx-bootstrap-demo"
 COMMENT_ID=-1
@@ -29,7 +29,7 @@ done
 
 if [[ ${COMMENT_ID} -ge 0 ]];
   then
-    curl -H "Authorization: token $GITHUB_TOKEN" -X PATCH -d "{\"body\": \"$NEW_COMMENT\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/comments/${COMMENT_ID}"
+    curl -H "Authorization: token ${GITHUB_TOKEN}" -X PATCH -d "{\"body\": \"$NEW_COMMENT\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/comments/${COMMENT_ID}"
   else
-    curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{\"body\": \"$NEW_COMMENT\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"$NEW_COMMENT\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
