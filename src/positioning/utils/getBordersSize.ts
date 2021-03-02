f6d23b18b46aa2ec2b0310c69512cdcf85f0ea97
Dmitriy Shekhovtsov <valorkin@gmail.com>
2021-03-02 13:17:15 +0200

    strict: progressbar and part of positioning

--- src/positioning/utils/getBordersSize.ts
+++ src/positioning/utils/getBordersSize.ts
@@ -2,12 +2,12 @@
  * Helper to detect borders of a given element
  */
 
-export function getBordersSize(styles: CSSStyleDeclaration, axis: string) {
+export function getBordersSize(styles: CSSStyleDeclaration, axis: string): number {
   const sideA = axis === 'x' ? 'Left' : 'Top';
   const sideB = sideA === 'Left' ? 'Right' : 'Bottom';
 
   return (
-    parseFloat(styles[`border${sideA}Width`]) +
-    parseFloat(styles[`border${sideB}Width`])
+    parseFloat((styles as never)[`border${sideA}Width`]) +
+    parseFloat((styles as never)[`border${sideB}Width`])
   );
 }
