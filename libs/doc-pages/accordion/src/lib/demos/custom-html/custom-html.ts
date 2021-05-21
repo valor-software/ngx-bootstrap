cbdbf8bc9553a09663c12e5b3a9a22c55a4d1960
vs <svetlana.muravlova@valor-software.com>
2021-05-21 15:43:57 +0300

    feat(accordion): updated classes docs-accrodion  according bs5

--- libs/doc-pages/accordion/src/lib/demos/custom-html/custom-html.ts
+++ libs/doc-pages/accordion/src/lib/demos/custom-html/custom-html.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { getBsVer } from 'ngx-bootstrap/utils';
+import { IBsVersion } from '@ngx-bootstrap-doc/docs';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'demo-accordion-custom-html',
   templateUrl: './custom-html.html'
 })
-export class DemoAccordionCustomHTMLComponent {}
+export class DemoAccordionCustomHTMLComponent {
+  get _getBsVer(): IBsVersion {
+    return getBsVer()
+  }
+}
