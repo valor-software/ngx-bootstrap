67736e09a9810d4beaddffe43af732b19fa5d7ee
SvetlanaMuravlova <83700465+SvetlanaMuravlova@users.noreply.github.com>
2021-07-16 17:14:05 +0300

    feat(bootstrap): updated bootstrap 4.0.0 up to 4.5.3 (#6206)

--- libs/common-docs/src/lib/demo-section-components/demo-examples-section/stackblitz/html.ts
+++ libs/common-docs/src/lib/demo-section-components/demo-examples-section/stackblitz/html.ts
@@ -2,7 +2,7 @@ import { isBs3 } from 'ngx-bootstrap/utils';
 import { NgxModuleData } from './app.module';
 
 export function getIndexHtmlCode(tag: string, component: NgxModuleData) {
-  return `<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/${isBs3() ? '3.3.7' : '4.0.0'}/css/bootstrap.min.css">
+  return `<link rel="stylesheet" href=${isBs3() ? "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" : "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"} crossorigin=${!isBs3() ? "anonymous" : undefined} integrity=${!isBs3() ? "sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" : undefined}>
 <link rel="stylesheet" href="https://unpkg.com/ngx-bootstrap/datepicker/bs-datepicker.css">
 <div class="card-header mb-2 well">
 This demo shows functionality of <strong>${component.moduleFolder}</strong> from <strong>ngx-bootstrap.</strong><br/>
