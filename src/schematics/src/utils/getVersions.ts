19b6c37da72c1fe73d3e3c10478c84d039be836b
SvetlanaMuravlova <83700465+SvetlanaMuravlova@users.noreply.github.com>
2021-07-19 14:23:51 +0300

    Fix (schematics): fixed dependency versions (#6210)
    * fix(dchematics): fixed dependency versions
    
    * fix(schematics): fixed dependency versions

--- src/schematics/src/utils/getVersions.ts
+++ src/schematics/src/utils/getVersions.ts
@@ -1,36 +1,20 @@
 import { normalize } from '@angular-devkit/core';
 import { Tree } from '@angular-devkit/schematics';
+import * as currentVersions from './current_dependency_versions.json';
 
-let NGX_BOOTSTRAP_VERSION = '7.0.0-rc.1';
-const BOOTSTRAP_VERSION = '^4.5.0';
-const dependencies = [
-  { name: 'bootstrap', version: BOOTSTRAP_VERSION},
-  { name: 'ngx-bootstrap', version: NGX_BOOTSTRAP_VERSION}
-];
+const NGX_BOOTSTRAP_VERSION = currentVersions.NGX_BOOTSTRAP_VERSION;
+const BOOTSTRAP_VERSION = currentVersions.BOOTSTRAP_VERSION;
+
 // Regular expression that matches all possible Angular CLI default style files
 const defaultStyleFileRegex = /styles\.(c|le|sc|sa)ss/;
 // Regular expression that matches all files that have a proper stylesheet extension
 const validStyleFileRegex = /\.(c|le|sc|sa)ss/;
 
 export function getDependencies(host: Tree): { name: string, version: string }[] {
-  if (host.exists('node_modules/ngx-bootstrap/schematics/package.json')) {
-    const sourceText = host.read('node_modules/ngx-bootstrap/schematics/package.json')?.toString('utf-8');
-    const json = JSON.parse(sourceText);
-    if (json.version) {
-      NGX_BOOTSTRAP_VERSION = json.version;
-      return updateVersionDependencies(json.version, 'ngx-bootstrap');
-    }
-  }
-  return dependencies;
-}
-
-function updateVersionDependencies(version: string, name: string): { name: string, version: string }[] {
-  dependencies.find(dep => {
-    if (dep.name === name) {
-      dep.version = version;
-    }
-  });
-  return  dependencies;
+  return [
+    { name: 'bootstrap', version: BOOTSTRAP_VERSION},
+    { name: 'ngx-bootstrap', version: NGX_BOOTSTRAP_VERSION}
+  ];
 }
 
 export function getProjectStyleFile(existingStyles: string[], extension?: string): string | null {
