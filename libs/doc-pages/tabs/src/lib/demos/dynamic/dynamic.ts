15d75499f76dd183c514168aba6699850205f2e8
svetlanaMuravlova <svetlana.muravlova@valor-software.com>
2021-07-26 17:08:01 +0300

    fix(tab): fix after review

--- libs/doc-pages/tabs/src/lib/demos/dynamic/dynamic.ts
+++ libs/doc-pages/tabs/src/lib/demos/dynamic/dynamic.ts
@@ -19,8 +19,10 @@ export class DemoTabsDynamicComponent {
   tabs: ITab[] = [
     { title: 'Dynamic Title 1', content: 'Dynamic content 1'},
     { title: 'Dynamic Title 2', content: 'Dynamic content 2'},
-    { title: 'Dynamic Title 3', content: 'Dynamic content 3', removable: true, customClass: '123'}
+    { title: 'Dynamic Title 3', content: 'Dynamic content 3', removable: true}
   ];
+
+  customClassExperimantal = 'test tes test'
 
   addNewTab(): void {
     const newTabIndex = this.tabs.length + 1;
