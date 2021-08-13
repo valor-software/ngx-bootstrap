dcddf8c07e1f48e3e17439d0299a73c043a35daf
svetlanaMuravlova <svetlana.muravlova@valor-software.com>
2021-08-13 15:50:34 +0300

    feat(storybook): deleted unite tests

--- src/root/.storybook/main.js
+++ src/root/.storybook/main.js
@@ -5,6 +5,5 @@ rootMain.core = { ...rootMain.core, builder: 'webpack5' };
 // Use the following syntax to add addons!
 // rootMain.addons.push('');
 rootMain.stories.push(...['./stories/*.stories.@(js|jsx|ts|tsx)']);
-rootMain.addons.push(...'@storybook/addon-jest');
 
 module.exports = rootMain;
