3f823d0fc84c894274603bc3bbb8c938a41cd691
YisusW <jesusochoa266@gmail.com>
2021-09-07 19:52:09 -0500

    test(datepicker):  date picker component onSlectionDone

--- src/datepicker/testing/datepicker.component.spec.ts
+++ src/datepicker/testing/datepicker.component.spec.ts
@@ -1,29 +1,15 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Component, ViewChild, OnInit } from '@angular/core';
 import { DatePickerComponent } from '../datepicker.component';
 import { DatepickerModule } from '../datepicker.module';
 
-@Component({
-  selector: 'test-cmp',
-  template: `<datepicker></datepicker>`
-})
-class TestComponent implements OnInit {
-  @ViewChild(DatePickerComponent, { static: true }) datepicker: DatePickerComponent;
-
-  ngOnInit(): void {
-    this.datepicker.writeValue(new Date());
-  }
-}
-
-type TestFixture = ComponentFixture<TestComponent>;
-
 describe('datepicker:', () => {
-  let fixture: TestFixture;
+  let fixture: ComponentFixture<DatePickerComponent>;
+  let component: DatePickerComponent;
 
   beforeEach(
     waitForAsync(() => TestBed.configureTestingModule({
-      declarations: [TestComponent],
+      declarations: [DatePickerComponent],
       imports: [
         DatepickerModule.forRoot(),
         BrowserAnimationsModule
@@ -31,12 +17,24 @@ describe('datepicker:', () => {
     }).compileComponents())
   );
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DatePickerComponent);
+    component = fixture.componentInstance;
+  });
+  
   it('should not throw undefined reference error when initializing value before content init hook',
     () => {
       expect(() => {
-        fixture = TestBed.createComponent(TestComponent);
         fixture.detectChanges();
       }).not.toThrowError(/^.*undefined.*$/gm);
     }
   );
+
+  it('should emit a new Date', () => {
+    const selectionDoneEmit = jest.spyOn(component.selectionDone, 'emit');
+
+    component.onSelectionDone(new Date);
+
+    expect(selectionDoneEmit).toHaveBeenCalled();
+  });
 });
