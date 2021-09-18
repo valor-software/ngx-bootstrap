bb21080d1d1038b0fd7084ac27f0f28db7e7b1d4
Vincent Guillou <vincent.g@padoa-group.com>
2021-09-18 19:23:40 +0200

    fix(popover) Fix, format and test 'computeAutoPlacement'

--- src/positioning/utils/computeAutoPlacement.ts
+++ src/positioning/utils/computeAutoPlacement.ts
@@ -6,7 +6,7 @@ import { getBoundaries } from './getBoundaries';
 import { Offsets, PlacementForBs5 } from '../models';
 import { getBsVer } from 'ngx-bootstrap/utils';
 
-function getArea({ width, height }: { width: number, height: number }) {
+function getArea({ width, height }: { width: number; height: number }) {
   return width * height;
 }
 
@@ -25,55 +25,54 @@ export function computeAutoPlacement(
 
   const boundaries = getBoundaries(target, host, padding, boundariesElement);
 
-  type Rects = {top: Offsets, right: Offsets, bottom: Offsets, left: Offsets};
+  type Rects = { top: Offsets; right: Offsets; bottom: Offsets; left: Offsets };
   const rects: Rects = {
     top: {
-      width: boundaries.width ?? 0,
-      height: refRect.top && boundaries.top ? refRect.top - boundaries.top : 0
+      width: boundaries?.width ?? 0,
+      height: (refRect?.top ?? 0) - (boundaries?.top ?? 0)
     },
     right: {
-      width: boundaries.right && refRect.right ? boundaries.right - refRect.right : 0,
-      height: boundaries.height ?? 0
+      width: (boundaries?.right ?? 0) - (refRect?.right ?? 0),
+      height: boundaries?.height ?? 0
     },
     bottom: {
-      width: boundaries.width ?? 0,
-      height: boundaries.bottom && refRect.bottom ? boundaries.bottom - refRect.bottom: 0
+      width: boundaries?.width ?? 0,
+      height: (boundaries?.bottom ?? 0) - (refRect?.bottom ?? 0)
     },
     left: {
-      width: refRect.left && boundaries.left ? refRect.left - boundaries.left : 0,
-      height: boundaries.height ?? 0
+      width: (refRect.left ?? 0) - (boundaries?.left ?? 0),
+      height: boundaries?.height ?? 0
     }
   };
 
   const sortedAreas = Object.keys(rects)
     .map((key) => ({
-      key,
+      position: key,
       ...rects[key as keyof Rects],
-      area: getArea(rects[key as keyof Rects] as {width: number, height: number})
+      area: getArea(rects[key as keyof Rects] as { width: number; height: number })
     }))
     .sort((a, b) => b.area - a.area);
 
-  let filteredAreas = sortedAreas.filter(
-    ({ width, height }) => {
-      return width >= target.clientWidth
-        && height >= target.clientHeight;
-    }
-  );
-
-  filteredAreas = filteredAreas.filter((position) => {
-    return allowedPositions
-      .some((allowedPosition: string) => {
-        return allowedPosition === position.key;
-      });
+  let filteredAreas = sortedAreas.filter(({ width, height }) => {
+    return width >= target.clientWidth && height >= target.clientHeight;
   });
 
-  const computedPlacement: string = filteredAreas.length > 0
-    ? filteredAreas[0].key
-    : sortedAreas[0].key;
+  filteredAreas = filteredAreas.filter(({ position }) => {
+    return allowedPositions.some((allowedPosition: string) => {
+      return allowedPosition === position;
+    });
+  });
+
+  const computedPlacement: string = filteredAreas.length > 0 ? filteredAreas[0].position : sortedAreas[0].position;
 
   const variation = placement.split(' ')[1];
   // for tooltip on auto position
-  target.className = target.className.replace(/bs-tooltip-auto/g, `bs-tooltip-${getBsVer().isBs5 ? PlacementForBs5[computedPlacement as keyof typeof PlacementForBs5] : computedPlacement}`);
+  target.className = target.className.replace(
+    /bs-tooltip-auto/g,
+    `bs-tooltip-${
+      getBsVer().isBs5 ? PlacementForBs5[computedPlacement as keyof typeof PlacementForBs5] : computedPlacement
+    }`
+  );
 
   return computedPlacement + (variation ? `-${variation}` : '');
 }
