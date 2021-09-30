25546d40c4362b22a9db6381a3da913cd4cc6de1
MuravlovaSvetlana <ushakovasvetlana94kh@gmail.com>
2021-09-30 13:53:45 +0300

    feat(sidebar): updated sidebar design

--- libs/common-docs/src/lib/common/search-input/search-input.component.ts
+++ libs/common-docs/src/lib/common/search-input/search-input.component.ts
@@ -1,44 +1,32 @@
-import { ActivatedRoute, NavigationEnd, Route, Router, Routes } from "@angular/router";
-import { Component, Inject, OnDestroy, Renderer2, Input } from "@angular/core";
+import { ActivatedRoute, Route, Router, Routes } from "@angular/router";
+import { Component, Inject, Input } from "@angular/core";
 import { DOCUMENT } from '@angular/common';
 import { DOCS_TOKENS } from '../../tokens/docs-routes-token';
-import { Subscription } from "rxjs";
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'search-input',
   templateUrl: './search-input.component.html'
 })
-export class SearchInputComponent implements OnDestroy{
+export class SearchInputComponent {
   @Input() showInput = true;
   isShown = false;
   routes: Routes;
   search = { text: '' };
-  // scrollSubscription: Subscription;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
     @Inject(DOCUMENT) private document: any,
     @Inject(DOCS_TOKENS) _routes: Routes,
-    private renderer: Renderer2
   ) {
     this.routes = _routes.filter((v: Route) => v.path !== '**');
-    // this.scrollSubscription = this.router.events.subscribe((event: any) => {
-    //   if (event instanceof NavigationEnd) {
-    //     this.toggle(false);
-    //   }
-    // });
   }
 
   preventReloading(event: KeyboardEvent) {
     if (event.keyCode === 13 || event.key === 'Enter') {
       event.preventDefault();
     }
-  }
-
-  ngOnDestroy() {
-    // this.scrollSubscription.unsubscribe();
   }
 }
 
