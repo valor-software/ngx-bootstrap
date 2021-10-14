82629a387ea25d641da79eb1878ce5599f68df56
MuravlovaSvetlana <ushakovasvetlana94kh@gmail.com>
2021-10-14 15:38:03 +0300

    feat(documentation): updated examples page (buttons)

--- libs/common-docs/src/lib/common/add-nav/add-nav.component.ts
+++ libs/common-docs/src/lib/common/add-nav/add-nav.component.ts
@@ -31,28 +31,27 @@ export class AddNavComponent implements OnChanges, AfterViewInit {
   @ViewChildren('scrollElement')
   private scrollElementsList?: QueryList<ElementRef>;
 
-  @ViewChild('container')
-  private addNavContainer?: ElementRef;
+  // @ViewChild('container')
+  // private addNavContainer?: ElementRef;
 
   _componentContent?: IComponentContent;
 
   @HostListener('window:scroll')
   onScrollEvent() {
-    if (this.addNavContainer) {
-      if (!this.addNavContainer.nativeElement.getAttribute('data-position')) {
-        setTimeout(() => {
-          this._renderer.setAttribute(this.addNavContainer?.nativeElement, 'data-position', (this.addNavContainer?.nativeElement.offsetTop - 64).toString());
-        }, 100);
-      }
-
-      this.initMenu();
-    }
+    // if (this.addNavContainer) {
+    //   if (!this.addNavContainer.nativeElement.getAttribute('data-position')) {
+    //       this._renderer.setAttribute(this.addNavContainer?.nativeElement, 'data-position', (this.addNavContainer?.nativeElement.offsetTop - 64).toString());
+    //   }
+    //
+    //   this.initMenu();
+    // }
 
     if (this.scrollElementsList?.length) {
       this.scrollElementsList.map(item => {
         const min = item.nativeElement.getAttribute('data-min-scroll-value');
         const max = item.nativeElement.getAttribute('data-max-scroll-value');
         const position = window.pageYOffset;
+        // console.log(position, min, max);
         if (position >= min && position <= max) {
           this._renderer.addClass(item.nativeElement.parentElement, 'active');
         } else {
@@ -69,25 +68,28 @@ export class AddNavComponent implements OnChanges, AfterViewInit {
     private router: Router
   ){}
 
-  initMenu() {
-    const attributeValue = this.addNavContainer?.nativeElement.getAttribute('data-position');
-    const elementOffsetTop = this.addNavContainer?.nativeElement.offsetTop - 64; // 64 - height of the header
-    if (!attributeValue && elementOffsetTop <= window.pageYOffset) {
-      this._renderer.addClass(this.addNavContainer?.nativeElement, 'fixed');
-    } else {
-      this._renderer.removeClass(this.addNavContainer?.nativeElement, 'fixed');
-    }
-
-    if (attributeValue && attributeValue <= window.pageYOffset) {
-      this._renderer.addClass(this.addNavContainer?.nativeElement, 'fixed');
-    } else {
-      this._renderer.removeClass(this.addNavContainer?.nativeElement, 'fixed');
-    }
-  }
+  // initMenu() {
+  //   const attributeValue = this.addNavContainer?.nativeElement.getAttribute('data-position');
+  //   const elementOffsetTop = this.addNavContainer?.nativeElement.offsetTop - 64; // 64 - height of the header
+  //   if (!attributeValue && elementOffsetTop <= window.pageYOffset) {
+  //     this._renderer.addClass(this.addNavContainer?.nativeElement, 'fixed');
+  //   } else {
+  //     this._renderer.removeClass(this.addNavContainer?.nativeElement, 'fixed');
+  //   }
+  //
+  //   if (attributeValue && attributeValue <= window.pageYOffset) {
+  //     this._renderer.addClass(this.addNavContainer?.nativeElement, 'fixed');
+  //   } else {
+  //     this._renderer.removeClass(this.addNavContainer?.nativeElement, 'fixed');
+  //   }
+  // }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes?.componentContent) {
       this._componentContent = this.mapComponentContent(changes.componentContent.currentValue);
+      setTimeout(()=> {
+        this.setScrollAttributes();
+      }, 100);
     }
   }
 
@@ -117,20 +119,24 @@ export class AddNavComponent implements OnChanges, AfterViewInit {
     }
   }
 
+  setScrollAttributes() {
+    const header: number = this.document.querySelector('header')?.offsetHeight || 0;
+    this.scrollElementsList?.map(item => {
+      const id = item.nativeElement.getAttribute('data-anchor');
+      const target: HTMLElement | null = this.document.getElementById(id);
+      if (target) {
+        const targetPosY: number = target.offsetTop - header - 6;
+        const parentHeight = (<HTMLElement>target.parentElement).getBoundingClientRect().height - 6 || 0;
+        this._renderer.setAttribute(item.nativeElement, 'data-max-scroll-value', (targetPosY + parentHeight).toString());
+        this._renderer.setAttribute(item.nativeElement, 'data-min-scroll-value', (targetPosY).toString());
+      }
+      return item;
+    });
+  }
+
   ngAfterViewInit() {
     setTimeout(() => {
-      const header: number = this.document.querySelector('header')?.offsetHeight || 0;
-      this.scrollElementsList?.map(item => {
-        const id = item.nativeElement.getAttribute('data-anchor');
-        const target: HTMLElement | null = this.document.getElementById(id);
-        if (target) {
-          const targetPosY: number = target.offsetTop - header - 6;
-          const parentHeight = (<HTMLElement>target.parentElement).getBoundingClientRect().height - 6 || 0;
-          this._renderer.setAttribute(item.nativeElement, 'data-max-scroll-value', (targetPosY + parentHeight).toString());
-          this._renderer.setAttribute(item.nativeElement, 'data-min-scroll-value', (targetPosY).toString());
-        }
-        return item;
-      });
+      this.setScrollAttributes();
     },100);
 
   }
