1dd3841eadebaf38911fc1e98eb369d03e16efd5
MuravlovaSvetlana <ushakovasvetlana94kh@gmail.com>
2021-11-12 11:12:42 +0200

    fix(tests): runable cypress, unit testt, tests should be fixed

--- src/pagination/jest.config.js
+++ src/pagination/jest.config.js
@@ -14,5 +14,7 @@ module.exports = {
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment'
   ],
-  transform: { '^.+\\.(ts|js|html)$': 'jest-preset-angular' }
+  transform: { '^.+.(ts|mjs|js|html)$': 'jest-preset-angular' },
+  transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
+  moduleFileExtensions: ['mjs', 'ts', 'js', 'html']
 };
