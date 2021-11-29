a7912a021ede26386efb4207ac37aa45f4667d94
Kevin Reid <kevin.reid@fourth.com>
2021-11-29 13:54:10 -0800

    feat(datepicker): add Spanish (Puerto Rico) [es-pr] locale

--- libs/doc-pages/datepicker/src/lib/demo-datepicker.module.ts
+++ libs/doc-pages/datepicker/src/lib/demo-datepicker.module.ts
@@ -7,7 +7,7 @@ import { defineLocale, LocaleData } from 'ngx-bootstrap/chronos';
 import { BsDatepickerModule, DatepickerModule } from 'ngx-bootstrap/datepicker';
 
 import {
-  arLocale, bgLocale, caLocale, csLocale, daLocale, deLocale, enGbLocale, esDoLocale, esLocale, esUsLocale, etLocale, frLocale, heLocale,
+  arLocale, bgLocale, caLocale, csLocale, daLocale, deLocale, enGbLocale, esDoLocale, esLocale, esPrLocale, esUsLocale, etLocale, frLocale, heLocale,
   hiLocale, hrLocale, fiLocale, glLocale, huLocale, idLocale, itLocale, jaLocale, kaLocale, kkLocale, koLocale, ltLocale, lvLocale, mnLocale, nbLocale,
   nlBeLocale, nlLocale, plLocale, ptBrLocale, ruLocale, roLocale, skLocale, slLocale, sqLocale, svLocale, thLocale, thBeLocale, trLocale, viLocale,
   zhCnLocale, ukLocale
@@ -21,7 +21,7 @@ import { routes } from './demo-datepicker.routes';
 import { DEMO_COMPONENTS } from './demos/index';
 
 const locales = [
-  arLocale, bgLocale, caLocale, csLocale, daLocale, deLocale, enGbLocale, esDoLocale, esLocale, esUsLocale, etLocale, frLocale,
+  arLocale, bgLocale, caLocale, csLocale, daLocale, deLocale, enGbLocale, esDoLocale, esLocale, esPrLocale, esUsLocale, etLocale, frLocale,
   heLocale, hiLocale, hrLocale, fiLocale, glLocale, huLocale, idLocale, itLocale, jaLocale, kaLocale, kkLocale, koLocale, ltLocale, lvLocale, mnLocale,
   nbLocale, nlBeLocale, nlLocale, plLocale, ptBrLocale, ruLocale, roLocale, skLocale, slLocale, sqLocale, svLocale, thLocale, thBeLocale,
   trLocale, ukLocale, viLocale, zhCnLocale
