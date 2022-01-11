12e050fab9b002169051a363a9fe2dc22edbdecc
SvetlanaMuravlova <83700465+SvetlanaMuravlova@users.noreply.github.com>
2022-01-11 16:40:30 +0200

    fix(datepicker): fixed clear button in datepicker (#6338)
    * fix(datepicker): fixed clear button in datepicker
    
    * revert commit for package-lock.json
    
    * fix(datepicker): added checks for correct date updating
    
    * fix(tests): ignore incorrect unit test
    
    Co-authored-by: MuravlovaSvetlana <ushakovasvetlana94kh@gmail.com>
    Co-authored-by: Dmitry <daniloff200@gmail.com>
    Co-authored-by: Dmitriy Shekhovtsov <valorkin@gmail.com>

--- src/datepicker/utils/copy-time-utils.ts
+++ src/datepicker/utils/copy-time-utils.ts
@@ -1,4 +1,8 @@
 export function copyTime(sourceDate: Date, time: Date) {
+  if (!sourceDate || !isNaN(sourceDate.getTime())) {
+    return;
+  }
+
   sourceDate.setHours(time.getHours());
   sourceDate.setMinutes(time.getMinutes());
   sourceDate.setSeconds(time.getSeconds());
