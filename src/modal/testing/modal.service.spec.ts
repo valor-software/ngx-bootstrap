533bf2f60a69561fad1dca94cbcb6b5ccc0ce789
Alejandro Lora <alejandrofpo@gmail.com>
2022-10-24 18:33:53 +0200

    (test:modal): bring back done & tests isolation

--- src/modal/testing/modal.service.spec.ts
+++ src/modal/testing/modal.service.spec.ts
@@ -37,19 +37,21 @@ describe('Modal service', () => {
     fixture.detectChanges();
   });
 
-  it('should return random id on spin up a new modal', () => {
+  it('should return random id on spin up a new modal', done => {
     modalService.onShown.subscribe((data) => {
       expect(data.id).toBeTruthy();
+      done();
     });
 
     modalService.show(TestModalComponent);
   });
 
-  it('should return different random id when open nested modal without specifying id', () => {
+  it('should return different random id when open nested modal without specifying id', done => {
     modalService.onShown.pipe(
       pairwise(),
       tap(([firstData, secondData]) => {
         expect(firstData.id).not.toBe(secondData.id);
+        done();
       })
     ).subscribe();
 
@@ -57,22 +59,24 @@ describe('Modal service', () => {
     modalService.show(TestModalComponent);
   });
 
-  it('should return id in config when specified', () => {
+  it('should return id in config when specified', done => {
     const id = 20;
 
     modalService.onShown.subscribe((data) => {
       expect(data.id).toBe(id);
+      done();
     });
 
     modalService.show(TestModalComponent, { id });
   });
 
-  it('should return id when hide modal', () => {
+  it('should return id when hide modal', done => {
     const id = 20;
     jest.useFakeTimers();
 
     modalService.onHidden.subscribe((data) => {
       expect(data.id).toBe(id);
+      done();
     });
 
     const bsRef = modalService.show(TestModalComponent, { id });
@@ -83,16 +87,24 @@ describe('Modal service', () => {
 
   it('should hide modal even if invoked right after show', () => {
     const id = 25;
-    const onHideSpy = jest.spyOn(modalService.onHide, 'emit');
-    const onHiddenSpy = jest.spyOn(modalService.onHidden, 'emit');
     jest.useFakeTimers();
 
-    modalService.onHidden.subscribe((data) => {
-      expect(data.id).toBe(id);
-    });
+    const bsRef = modalService.show(TestModalComponent, { id });
+    expect(modalService.getModalsCount()).toBe(1);
+    bsRef.hide();
+
+    jest.runAllTimers();
+    expect(modalService.getModalsCount()).toBe(0);
+  });
+
+  it('should emit onHide after hide is invoked', done => {
+    const id = 25;
+    const onHideSpy = jest.spyOn(modalService.onHide, 'emit');
+    jest.useFakeTimers();
 
     modalService.onHide.subscribe((data) => {
       expect(data.id).toBe(id);
+      done();
     });
 
     const bsRef = modalService.show(TestModalComponent, { id });
@@ -100,6 +112,22 @@ describe('Modal service', () => {
 
     jest.runAllTimers();
     expect(onHideSpy).toHaveBeenCalled();
+  });
+
+  it('should emit (id) onHidden after hide is invoked', done => {
+    const id = 25;
+    const onHiddenSpy = jest.spyOn(modalService.onHidden, 'emit');
+    jest.useFakeTimers();
+
+    modalService.onHidden.subscribe((data) => {
+      expect(data.id).toBe(id);
+      done();
+    });
+
+    const bsRef = modalService.show(TestModalComponent, { id });
+    bsRef.hide();
+
+    jest.runAllTimers();
     expect(onHiddenSpy).toHaveBeenCalledWith({ id });
   });
 });
