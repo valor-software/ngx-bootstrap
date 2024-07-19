7d2e69740106a806c4bdfbd6ba132666bb5ac351
Umanskiy Aleksey <sharkakatsukie@gmail.com>
2024-07-19 11:25:23 +0300

    feat(typeahead): added moduleless support

--- src/typeahead/typeahead.module.ts
+++ src/typeahead/typeahead.module.ts
@@ -1,21 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { TypeaheadContainerComponent } from './typeahead-container.component';
 import { TypeaheadDirective } from './typeahead.directive';
-import { ComponentLoaderFactory } from 'ngx-bootstrap/component-loader';
-import { PositioningService } from 'ngx-bootstrap/positioning';
 
 @NgModule({
-    imports: [CommonModule],
-    declarations: [TypeaheadContainerComponent, TypeaheadDirective],
+    imports: [CommonModule, TypeaheadContainerComponent, TypeaheadDirective],
     exports: [TypeaheadContainerComponent, TypeaheadDirective]
 })
-export class TypeaheadModule {
-  static forRoot(): ModuleWithProviders<TypeaheadModule> {
-    return {
-      ngModule: TypeaheadModule,
-      providers: [ComponentLoaderFactory, PositioningService]
-    };
-  }
-}
+export class TypeaheadModule {}
