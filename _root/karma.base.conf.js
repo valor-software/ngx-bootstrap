6997707395d402cc388ec4aad7ea58409f13323a
Umanskiy Aleksey <sharkakatsukie@gmail.com>
2024-12-13 17:47:41 +0200

    feat(ssr-docs); merged development

--- _root/karma.base.conf.js
+++ _root/karma.base.conf.js
@@ -36,7 +36,7 @@ module.exports = function(projectConfig) {
       browsers: ['ChromeHeadless'],
       browserNoActivityTimeout: 20000,
       browserDisconnectTolerance: 2,
-      browserDisconnectTimeout: 5000,
+      browserDisconnectTimeout: 10000,
       singleRun: true,
       customLaunchers: {
         Chrome_travis_ci: {
