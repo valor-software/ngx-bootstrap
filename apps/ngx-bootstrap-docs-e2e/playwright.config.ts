50d7e70db8d6b729904e09a32b32f98bc7ecee87
Umanskiy Aleksey <sharkakatsukie@gmail.com>
2024-12-16 11:01:18 +0200

    feat(ssr-docs); merged development

--- apps/ngx-bootstrap-docs-e2e/playwright.config.ts
+++ apps/ngx-bootstrap-docs-e2e/playwright.config.ts
@@ -5,7 +5,9 @@ export default defineConfig({
   testMatch: '**/*.spec.ts',
   fullyParallel: true,
   workers: 3,
-  timeout: 30000,
+  expect: {
+    timeout: 30000
+  },
   reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
   use: {
     baseURL: process.env['PLAYWRIGHT_TEST_BASE_URL'] || 'http://localhost:4200/ngx-bootstrap/',
