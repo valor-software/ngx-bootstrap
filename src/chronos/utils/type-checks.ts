70620ba312cae34e2bc0fff9a91569e663a0e64e
Umanskiy Aleksey <sharkakatsukie@gmail.com>
2025-07-02 15:42:37 +0300

    chore(docs): updated docs tabset to work properly in v20

--- src/chronos/utils/type-checks.ts
+++ src/chronos/utils/type-checks.ts
@@ -17,6 +17,7 @@ export function isDateValid(date: Date): boolean {
 }
 
 // eslint-disable-next-line @typescript-eslint/ban-types
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export function isFunction(fn: any): fn is Function {
   return (
     fn instanceof Function ||
