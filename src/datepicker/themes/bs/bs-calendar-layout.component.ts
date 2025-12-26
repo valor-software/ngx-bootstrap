85bd23aff8fd934cd2570febfd2bb2a1296415de
Amine Chergui <chergui.amine@gmail.com>
2025-12-26 14:25:58 +0100

    chore: migrate templates to  built-in control flow

--- src/datepicker/themes/bs/bs-calendar-layout.component.ts
+++ src/datepicker/themes/bs/bs-calendar-layout.component.ts
@@ -1,27 +1,31 @@
 import { Component } from '@angular/core';
 import { BsTimepickerViewComponent } from './bs-timepicker-view.component';
 import { BsCurrentDateViewComponent } from './bs-current-date-view.component';
-import { NgIf } from '@angular/common';
+
 
 @Component({
     selector: 'bs-calendar-layout',
     template: `
     <!-- current date, will be added in nearest releases -->
-    <bs-current-date title="hey there" *ngIf="false"></bs-current-date>
-
+    @if (false) {
+      <bs-current-date title="hey there"></bs-current-date>
+    }
+    
     <!--navigation-->
     <div class="bs-datepicker-head">
       <ng-content select="bs-datepicker-navigation-view"></ng-content>
     </div>
-
+    
     <div class="bs-datepicker-body">
       <ng-content></ng-content>
     </div>
-
+    
     <!--timepicker-->
-    <bs-timepicker *ngIf="false"></bs-timepicker>
-  `,
+    @if (false) {
+      <bs-timepicker></bs-timepicker>
+    }
+    `,
     standalone: true,
-    imports: [NgIf, BsCurrentDateViewComponent, BsTimepickerViewComponent]
+    imports: [BsCurrentDateViewComponent, BsTimepickerViewComponent]
 })
 export class BsCalendarLayoutComponent {}
