2469a92498c01d1b1d719ea24ce94ba1403097cf
Amine Chergui <chergui.amine@gmail.com>
2025-12-28 09:40:25 +0100

    test(typeahead): update Jest 'toThrowError' to 'toThrow' matcher

--- src/typeahead/testing/typeahead.directive.spec.ts
+++ src/typeahead/testing/typeahead.directive.spec.ts
@@ -184,7 +184,7 @@ describe('Directive: Typeahead', () => {
       fixture.detectChanges();
 
       fixture.whenStable().then(() => {
-        expect(() => directive.onBlur()).not.toThrowError();
+        expect(() => directive.onBlur()).not.toThrow();
       });
     }));
   });
@@ -676,21 +676,21 @@ describe('Directive: Typeahead', () => {
       directive.typeaheadWordDelimiters = ',';
       fixture.detectChanges();
       tick(100);
-      expect(() => directive.ngOnInit()).toThrowError();
+      expect(() => directive.ngOnInit()).toThrow();
     }));
 
     it('and use comma for typeaheadPhraseDelimiters, should throw error', fakeAsync(() => {
       directive.typeaheadPhraseDelimiters = ',';
       fixture.detectChanges();
       tick(100);
-      expect(() => directive.ngOnInit()).toThrowError();
+      expect(() => directive.ngOnInit()).toThrow();
     }));
 
     it('and use space for typeaheadMultipleSearchDelimiters, should throw error', fakeAsync(() => {
       directive.typeaheadMultipleSearchDelimiters = ' ';
       fixture.detectChanges();
       tick(100);
-      expect(() => directive.ngOnInit()).toThrowError();
+      expect(() => directive.ngOnInit()).toThrow();
     }));
 
     it("use space for typeaheadMultipleSearchDelimiters and ',' for typeaheadWordDelimiters, should not throw error", fakeAsync(() => {
@@ -698,7 +698,7 @@ describe('Directive: Typeahead', () => {
       directive.typeaheadWordDelimiters = ',';
       fixture.detectChanges();
       tick(100);
-      expect(() => directive.ngOnInit()).not.toThrowError();
+      expect(() => directive.ngOnInit()).not.toThrow();
     }));
 
     it('and use space for typeaheadMultipleSearchDelimiters and typeaheadSingleWords is false, should not throw error', fakeAsync(() => {
@@ -706,7 +706,7 @@ describe('Directive: Typeahead', () => {
       directive.typeaheadSingleWords = false;
       fixture.detectChanges();
       tick(100);
-      expect(() => directive.ngOnInit()).not.toThrowError();
+      expect(() => directive.ngOnInit()).not.toThrow();
     }));
   });
 });
