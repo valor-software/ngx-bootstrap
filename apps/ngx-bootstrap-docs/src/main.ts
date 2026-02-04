bb9861b41d69aa3263e616351159fe1c2499d1bc
Derek Liang <derek.liang@worksafebc.com>
2026-02-04 18:54:44 +0000

    refactor: remove zone change detection from bootstrap module and update event handling in dropdown demo

--- apps/ngx-bootstrap-docs/src/main.ts
+++ apps/ngx-bootstrap-docs/src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -9,5 +9,5 @@ if (environment.production) {
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
+  .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
